1f6dcf4a197cf57e91ddfbfc5927878ebfb6208b
Tivkaa Luper <tivkaaluper@gmail.com>
2018-11-19 15:54:00 +0100

    implemented dynamic values passed from Home controller

--- resources/views/Home/about.blade.php
+++ resources/views/Home/about.blade.php
@@ -1,7 +1,7 @@
 @extends('Layouts.app')
 
 @section('content')
-<h1>This is an about view from the Home folder</h1>
+<h1>{{$tile}}</h1>
 <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p> 
 @endsection
    
--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -1,5 +1,5 @@
 @extends('Layouts.app')
 @section('content')
-    <h1>My Home</h1>
+    <h1>{{$tile}}</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, hic voluptates asperiores laudantium ipsa iste magni libero consectetur sed. Dicta delectus earum minima quibusdam iste quia ullam, amet voluptatibus obcaecati.</p>
 @endsection

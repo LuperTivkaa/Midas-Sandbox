0a7b2b83044a32daaf283dbfbc73ca6dcedb2e51
Tivkaa Luper <tivkaaluper@gmail.com>
2018-11-19 14:46:20 +0100

    Added method in the Home controller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,5 +10,24 @@ class HomeController extends Controller
     public function index (){
         return view('Home.home');
     }
-    
+    //about method
+    public function about (){
+        return view('Home.about');
+    }
+
+    //Steering Committtee
+    public function committee (){
+        return view('Home.committee');
+    }
+
+    //Board
+    public function board (){
+        return view('Home.board');
+    }
+
+    //products
+    public function products (){
+        return view('Home.products');
+    }
+
 }

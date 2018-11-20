aea61507ccc5a7efd18aaaaf968efa38cef58d93
Tivkaa Luper <tivkaaluper@gmail.com>
2018-11-20 15:20:59 +0100

    Added personalised layout and content

--- resources/views/Home/committee.blade.php
+++ resources/views/Home/committee.blade.php
@@ -1,6 +1,130 @@
 @extends('Layouts.app')
-
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid sed vitae quo, reprehenderit recusandae autem aliquam tempore architecto magni placeat iste! Ullam, culpa temporibus? Quisquam inventore distinctio amet beatae quasi.</p>
+    <h1>Steering Committee</h1>
+    <div class="row">
+            <div class="col-sm-12 col-md-3 col-lg-3 mb-3 text-center ">
+      
+            </div>
+    
+            <div class="col-sm-12  col-md-6 col-lg-6 mb-3 text-center">
+    
+                <div class="card text-center">
+                    <img class="card-img-top" src="{{asset('images/andy.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mr. SHIMAKAA, iorlumun</h5>
+                        <h6 class="lead">President</h6>
+                        <p class="card-text">He is a clinical psychologist in the department of Clinical Psychology.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">ashimakaa@gmail.com</small>
+                        <small class="text-muted">+234 08063899921</small>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-sm-12  col-md-3 col-lg-3 text-center">
+    
+            </div>
+        </div>
+    
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+    
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/sesugh.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mr. SESUGH, denen</h5>
+                        <h6 class="lead">Secretary</h6>
+                        <p class="card-text">He is a nurse in the department of Ophthalmology.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">sesden2003@gmail.com</small>
+                        <small class="text-muted">+234 08069401427</small>
+                    </div>
+                </div>
+    
+            </div>
+    
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/edigah.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mrs. EDIGAH, felicia. </h5>
+                        <h6 class="lead">Member</h6>
+                        <p class="card-text">Works in the counselling unit of the APIN unit.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">edigahfelicia@gmail.com</small>
+                        <small class="text-muted">+234 07035389822</small>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/ternenge.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mr. TOROUGH, ternenge daniel, ACA</h5>
+                        <h6 class="lead">Member</h6>
+                        <p class="card-text">He is a chartered accountant with ICANN and works in the finance department.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">ter4humanity@gmail.com</small>
+                        <small class="text-muted">+234 08065310831</small>
+                    </div>
+                </div>
+            </div>
+    
+        </div>
+    
+        <div class="row">
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/apaa.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mr. APAA, tersoo</h5>
+                        <h6 class="lead">Member</h6>
+                        <p class="card-text">Works in the data office, APIN Complex.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">tergoodman@yahoo.com</small>
+                        <small class="text-muted">+234 08069665303</small>
+                    </div>
+                </div>
+            </div>
+    
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/uche.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Dr. UCHE, augustine azuka</h5>
+                        <h6 class="lead">Member</h6>
+                        <p class="card-text">A consultant gynaecologist in the department of Obsterics and Gynaecology.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">drucheazuka@gmail.com</small>
+                        <small class="text-muted">+234 08035976505</small>
+                    </div>
+                </div>
+    
+            </div>
+    
+            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+                <div class="card">
+                    <img class="card-img-top" src="{{asset('images/eronini.jpg')}}" alt="Card image cap">
+                    <div class="card-body text-center">
+                        <h5 class="card-title">Mrs. ERONINI, chiedozie</h5>
+                        <h6 class="lead">Member</h6>
+                        <p class="card-text">A laboratory scientist in the Department of Medical Laboratory.</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">eronininneoma7@gmail.com, mumuh12@yahoo.com</small>
+                        <small class="text-muted">+234 07067415598,, 08027880708</small>
+                    </div>
+                </div>
+    
+            </div>
+    
+        </div>
+    
 @endsection

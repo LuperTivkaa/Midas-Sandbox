17c97637d6380588b063121ee8756eb601da6c0f
Tivkaa Luper <tivkaaluper@gmail.com>
2018-11-25 20:22:41 +0100

    added scripts linls

--- resources/views/Layouts/app.blade.php
+++ resources/views/Layouts/app.blade.php
@@ -5,10 +5,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
     <link href="{{asset('fontawesome-assets/css/fontawesome.css')}}" rel="stylesheet">
     <link href="{{asset('fontawesome-assets/css/brands.css')}}" rel="stylesheet">
     <link href="{{asset('fontawesome-assets/css/solid.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
     <title>MIDAS APP</title>
 </head>
 
@@ -50,7 +51,18 @@
             </footer>
         </div>
     </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+        crossorigin="anonymous"></script>
+    {{--
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.5/popper.min.js"></script> --}}
+
     <script src="{{asset('js/app.js')}}"></script>
+
+    <script>
+
+    </script>
 </body>
 
 </html>

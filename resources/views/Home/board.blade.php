df7c5365e2955fa9b5d63990c0dd4d36e6952fbd
Tivkaa Luper <tivkaaluper@gmail.com>
2018-11-25 21:04:15 +0100

    Added responsive img styles

--- resources/views/Home/board.blade.php
+++ resources/views/Home/board.blade.php
@@ -1,82 +1,81 @@
-@extends('Layouts.app')
+@extends('Layouts.app') 
 @section('content')
 <h1>MIDAS Board</h1>
 <div class="row">
-        <div class="col-sm-12 col-md-3 col-lg-3 mb-3 text-center ">
-            {{-- empty div --}}
-        </div>
+    <div class="col-sm-12 col-md-3 col-lg-3 mb-3 text-center ">
+        {{-- empty div --}}
+    </div>
 
-        <div class="col-sm-12  col-md-6 col-lg-6 mb-3 text-center">
+    <div class="col-sm-12  col-md-6 col-lg-6 mb-3 text-center">
 
-            <div class="card">
-            <img class="card-img-top" src="{{asset('images/peteru.jpg')}}" alt="Card image cap">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Dr. Peteru Inunduh</h5>
-                    <h6 class="lead">Chairman</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/peteru.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Dr. Peteru Inunduh</h5>
+                <h6 class="lead">Chairman</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-        </div>
-
-        <div class="col-sm-12  col-md-3 col-lg-3 text-center">
-            {{-- empty div --}}
         </div>
     </div>
 
-    <div class="row">
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+    <div class="col-sm-12  col-md-3 col-lg-3 text-center">
+        {{-- empty div --}}
+    </div>
+</div>
 
-            <div class="card">
-                <img class="card-img-top" src="{{asset('images/orpin.jpg')}}" alt="Card image cap">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Mr. Manasseh Orpin</h5>
-                    <h6 class="lead">Member</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                
+<div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/orpin.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Mr. Manasseh Orpin</h5>
+                <h6 class="lead">Member</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
 
-        </div>
-
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-            <div class="card">
-                <img class="card-img-top" src="{{asset('images/joe.jpg')}}" alt="Card image cap">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Mr. Iorumbur Joseph Igbawua, Ph.D </h5>
-                    <h6 class="lead">Member</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                
-            </div>
-        </div>
-
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-            <div class="card">
-                <img class="card-img-top" src="{{asset('images/mfe.jpg')}}" alt="Card image cap">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Mr. Mfe Inga</h5>
-                    <h6 class="lead">Member</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-                
-            </div>
         </div>
 
     </div>
 
-    <div class="row">
-        <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-            <div class="card">
-                <img class="card-img-top" src="{{asset('images/enonche.jpg')}}" alt="Card image cap">
-                <div class="card-body text-center">
-                    <h5 class="card-title">Mrs. Patricia E. Enonche</h5>
-                    <h6 class="lead">Member</h6>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                </div>
-               
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/joe.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Mr. Iorumbur Joseph Igbawua, Ph.D </h5>
+                <h6 class="lead">Member</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
-        </div>
 
+        </div>
     </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/mfe.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Mr. Mfe Inga</h5>
+                <h6 class="lead">Member</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+
+        </div>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/enonche.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title">Mrs. Patricia E. Enonche</h5>
+                <h6 class="lead">Member</h6>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+
+        </div>
+    </div>
+
+</div>
 @endsection
-    
--- resources/views/Home/committee.blade.php
+++ resources/views/Home/committee.blade.php
@@ -1,130 +1,129 @@
-@extends('Layouts.app')
+@extends('Layouts.app') 
 @section('content')
-    <h1>Steering Committee</h1>
-    <div class="row">
-            <div class="col-sm-12 col-md-3 col-lg-3 mb-3 text-center ">
-      
+<h1>Steering Committee</h1>
+<div class="row">
+    <div class="col-sm-12 col-md-3 col-lg-3 mb-3 text-center ">
+
+    </div>
+
+    <div class="col-sm-12  col-md-6 col-lg-6 mb-3 text-center">
+
+        <div class="card text-center">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/andy.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mr. SHIMAKAA, iorlumun</h5>
+                <h6 class="text-muted">President</h6>
+                <p class="card-text">He is a clinical psychologist in the department of Clinical Psychology.</p>
             </div>
-    
-            <div class="col-sm-12  col-md-6 col-lg-6 mb-3 text-center">
-    
-                <div class="card text-center">
-                    <img class="card-img-top" src="{{asset('images/andy.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mr. SHIMAKAA, iorlumun</h5>
-                        <h6 class="text-muted">President</h6>
-                        <p class="card-text">He is a clinical psychologist in the department of Clinical Psychology.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">ashimakaa@gmail.com</small>
-                        <small class="text-muted">+234 08063899921</small>
-                    </div>
-                </div>
-            </div>
-    
-            <div class="col-sm-12  col-md-3 col-lg-3 text-center">
-    
+            <div class="card-footer">
+                <small class="text-muted">ashimakaa@gmail.com</small>
+                <small class="text-muted">+234 08063899921</small>
             </div>
         </div>
-    
-        <div class="row">
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-    
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/sesugh.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mr. SESUGH, denen</h5>
-                        <h6 class="text-muted">Secretary</h6>
-                        <p class="card-text">He is a nurse in the department of Ophthalmology.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">sesden2003@gmail.com</small>
-                        <small class="text-muted">+234 08069401427</small>
-                    </div>
-                </div>
-    
+    </div>
+
+    <div class="col-sm-12  col-md-3 col-lg-3 text-center">
+
+    </div>
+</div>
+
+<div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/sesugh.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mr. SESUGH, denen</h5>
+                <h6 class="text-muted">Secretary</h6>
+                <p class="card-text">He is a nurse in the department of Ophthalmology.</p>
             </div>
-    
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/edigah.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mrs. EDIGAH, felicia. </h5>
-                        <h6 class="text-muted">Member</h6>
-                        <p class="card-text">Works in the counselling unit of the APIN unit.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">edigahfelicia@gmail.com</small>
-                        <small class="text-muted">+234 07035389822</small>
-                    </div>
-                </div>
+            <div class="card-footer">
+                <small class="text-muted">sesden2003@gmail.com</small>
+                <small class="text-muted">+234 08069401427</small>
             </div>
-    
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/ternenge.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mr. TOROUGH, ternenge daniel, ACA</h5>
-                        <h6 class="text-muted">Member</h6>
-                        <p class="card-text">He is a chartered accountant with ICANN and works in the finance department.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">ter4humanity@gmail.com</small>
-                        <small class="text-muted">+234 08065310831</small>
-                    </div>
-                </div>
-            </div>
-    
         </div>
-    
-        <div class="row">
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/apaa.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mr. APAA, tersoo</h5>
-                        <h6 class="text-muted">Member</h6>
-                        <p class="card-text">Works in the data office, APIN Complex.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">tergoodman@yahoo.com</small>
-                        <small class="text-muted">+234 08069665303</small>
-                    </div>
-                </div>
+
+    </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/edigah.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mrs. EDIGAH, felicia. </h5>
+                <h6 class="text-muted">Member</h6>
+                <p class="card-text">Works in the counselling unit of the APIN unit.</p>
             </div>
-    
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/uche.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Dr. UCHE, augustine azuka</h5>
-                        <h6 class="text-muted">Member</h6>
-                        <p class="card-text">A consultant gynaecologist in the department of Obsterics and Gynaecology.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">drucheazuka@gmail.com</small>
-                        <small class="text-muted">+234 08035976505</small>
-                    </div>
-                </div>
-    
+            <div class="card-footer">
+                <small class="text-muted">edigahfelicia@gmail.com</small>
+                <small class="text-muted">+234 07035389822</small>
             </div>
-    
-            <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
-                <div class="card">
-                    <img class="card-img-top" src="{{asset('images/eronini.jpg')}}" alt="Card image cap">
-                    <div class="card-body text-center">
-                        <h5 class="card-title font-weight-bold">Mrs. ERONINI, chiedozie</h5>
-                        <h6 class="text-muted">Member</h6>
-                        <p class="card-text">A laboratory scientist in the Department of Medical Laboratory.</p>
-                    </div>
-                    <div class="card-footer">
-                        <small class="text-muted">eronininneoma7@gmail.com, mumuh12@yahoo.com</small>
-                        <small class="text-muted">+234 07067415598,, 08027880708</small>
-                    </div>
-                </div>
-    
-            </div>
-    
         </div>
-    
+    </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/ternenge.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mr. TOROUGH, ternenge daniel, ACA</h5>
+                <h6 class="text-muted">Member</h6>
+                <p class="card-text">He is a chartered accountant with ICANN and works in the finance department.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">ter4humanity@gmail.com</small>
+                <small class="text-muted">+234 08065310831</small>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<div class="row">
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/apaa.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mr. APAA, tersoo</h5>
+                <h6 class="text-muted">Member</h6>
+                <p class="card-text">Works in the data office, APIN Complex.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">tergoodman@yahoo.com</small>
+                <small class="text-muted">+234 08069665303</small>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/uche.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Dr. UCHE, augustine azuka</h5>
+                <h6 class="text-muted">Member</h6>
+                <p class="card-text">A consultant gynaecologist in the department of Obsterics and Gynaecology.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">drucheazuka@gmail.com</small>
+                <small class="text-muted">+234 08035976505</small>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="col-sm-12 col-md-4 col-lg-4 mb-3">
+        <div class="card">
+            <img class="card-img-top img-fluid cards-img" src="{{asset('images/eronini.jpg')}}" alt="Card image cap">
+            <div class="card-body text-center">
+                <h5 class="card-title font-weight-bold">Mrs. ERONINI, chiedozie</h5>
+                <h6 class="text-muted">Member</h6>
+                <p class="card-text">A laboratory scientist in the Department of Medical Laboratory.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">eronininneoma7@gmail.com, mumuh12@yahoo.com</small>
+                <small class="text-muted">+234 07067415598,, 08027880708</small>
+            </div>
+        </div>
+
+    </div>
+
+</div>
 @endsection

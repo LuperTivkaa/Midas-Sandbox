a6938b09c851f8fc8f87355309861dcc4d88d07a
Tivkaa Luper <tivkaaluper@gmail.com>
2018-12-29 21:00:09 +0100

    Removed unique modifier on username

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->default('midastouch')->unique();
+            $table->string('username')->default('midastouch');
             $table->integer('payment_number')->unique();
             $table->string('password');
             $table->string('email')->unique();

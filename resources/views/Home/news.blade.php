bffe770dc29a7f8fbb30ceb66b427aa23d2dfe75
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-07 09:29:32 +0100

    added new news view

---
+++ resources/views/Home/news.blade.php
@@ -0,0 +1,30 @@
+@extends('Layouts.app') 
+@section('content')
+<h1>Latest News</h1>
+<span>12/12/2018</span>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus corrupti quod modi dolore deserunt necessitatibus tenetur
+    laboriosam, earum, rem eligendi nihil tempore animi vero nostrum iure reprehenderit officiis doloremque. Minima.</p>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus odit beatae nulla reprehenderit earum, consectetur exercitationem
+    provident iusto. Corporis nemo porro, officiis debitis rem eveniet. Velit fugit, earum soluta, consectetur, ipsam doloribus
+    modi voluptate aut sunt maxime ducimus vitae alias. Ratione id itaque similique tenetur, suscipit aliquid nesciunt? Est,
+    molestiae?
+</p>
+
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio laboriosam itaque placeat recusandae ratione beatae nostrum
+    doloribus hic qui. A harum nemo rerum perspiciatis, eveniet nostrum fugiat eligendi autem ea, veritatis pariatur animi
+    officia? Consequatur libero sapiente animi neque atque nulla tempora facere provident? Perspiciatis, eius optio? Iste
+    pariatur nobis distinctio sed aperiam amet dicta ducimus odio aut quam quasi, minus in magnam tenetur quae! Temporibus,
+    rerum quam perspiciatis tempore asperiores nisi fugiat reprehenderit minus sapiente aut reiciendis laudantium, animi
+    labore eveniet pariatur beatae architecto harum eius ex! Unde doloremque laborum, placeat praesentium laboriosam consectetur
+    non natus. Repudiandae, quisquam ullam.</p>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, voluptates voluptate eum voluptas rerum natus suscipit neque
+    ratione alias quas numquam ullam eligendi, deserunt nam reprehenderit, distinctio corrupti laudantium vel autem officia
+    debitis. Culpa a assumenda neque in, odit officiis debitis, ipsa reiciendis cumque similique molestias dolore omnis aperiam
+    quae sequi, necessitatibus alias eos natus illo accusantium? Reprehenderit sapiente nobis, aliquam dolores excepturi
+    amet, obcaecati maxime rem aliquid error tempora provident? Quisquam facilis dolorum vero accusamus ullam officiis perferendis
+    atque dolorem! Minima delectus totam ullam rem quas, placeat praesentium debitis quibusdam rerum animi reprehenderit
+    suscipit vitae excepturi unde dolor? Rem sit, totam expedita ipsum dignissimos distinctio repellendus quidem enim laudantium
+    debitis sunt omnis quia provident nisi. Reiciendis doloribus libero quaerat esse sunt molestiae facere totam tenetur
+    soluta est, dolorem ab odio magnam sit eos voluptates quos omnis? Quas deleniti vero quis rerum. Nesciunt ab reprehenderit
+    dolor possimus, quisquam ipsam! Reiciendis.</p>
+@endsection

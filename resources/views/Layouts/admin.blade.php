2f8823455a623a98c3e200cbe4fa49d7b7312acf
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-08 14:57:57 +0100

    admin layout page

--- resources/views/Layouts/admin.blade.php
+++ resources/views/Layouts/admin.blade.php
@@ -5,14 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?famaily=Open+Sans:300,400,600">
+    <link rel="stylesheet" href="{{asset('css/portal.css')}}">
     <title>MIDAS- Admin Dashboard:: {{$title}}</title>
 </head>
 
 <body>
-    <div class="container">
-        @yield('admin-content')
-    </div>
 
+    @yield('admin')
+    <script src="{{asset('js/app.js')}}"></script>
 </body>
 
 </html>

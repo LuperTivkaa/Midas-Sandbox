af66d8c9802108746688629b396a2a9055df45b2
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-17 11:25:48 +0100

    changed classes to use material css

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -1,38 +1,31 @@
 @extends('Layouts.app') 
 @section('content')
 <div class="row">
-    <div class="col-sm-12 text-center">
-        <h4 class="display-1">MIDAS</h4>
-        <hr/>
-        <p class=" display-4 midas-banner-caption">Together, We Can Build The Future We Desire.</p>
-        <h6 class="lead">Desire, Innovate, Build</h6>
+    <div class="col s12 center-align">
+        <h1>MIDAS</h1>
+
+        <p class="midas-banner-caption">Together, We Can Build The Future We Desire.</p>
+        <h6>Desire, Innovate, Build</h6>
     </div>
 </div>
 </div>
-
-<div class="container text-center">
-    {{--
-    <h1>Our Creed!</h1>
-    <h4 class="lead">Friendship, Team Work, Service </h4> --}}
+{{-- core value section--}}
+<div class="container center-align">
     <div class="row">
-        <div class="col-sm-2 text-capitalize">
-            {{-- empty div --}}
+
+        <div class="col-s12 midas-values">
+            <img src="{{asset('images/Mission_Vision.png')}}" class="responsive-img" />
         </div>
-        <div class="col-sm-8 text-center">
-            <img src="{{asset('images/Mission_Vision.png')}}" class="img-fluid" />
-        </div>
-        <div class="col-sm-2 text-center">
-            {{-- empty div --}}
-        </div>
+
     </div>
 </div>
 
 <div class="container">
-    <h1 class="text-center  display-2">Our Products</h1>
+    <h1 class="center-align">Our Products</h1>
 
-    <div class="row text-center">
-        <div class="col-md-4">
-            <h4 class="text-muted">Loans</h4>
+    <div class="row center-align">
+        <div class="col s12 m4 l4">
+            <h4 class="grey-text">Loans</h4>
             <p>
                 We have carefully loan products that are flexible, sustainable and painless that can help all members to access various financial
                 services when eligible. Our loan catalogue is made of long term, short term and emergency loans at various
@@ -40,8 +33,8 @@
             </p>
 
         </div>
-        <div class="col-md-4">
-            <h4 class="text-muted">Bam</h4>
+        <div class="col s12 m4 l4">
+            <h4 class="grey-text">Target Savings</h4>
             <p>
                 Our Bam is a targeted saving product designed to allow members of the cooperative save money towards recuurent expenditures
                 like school during new academic savings. The saving is not specifically for school fees but can also be applied
@@ -49,35 +42,35 @@
             </p>
 
         </div>
-        <div class="col-md-4">
-            <h4 class="text-muted">Fixed Deposits</h4>
+        <div class="col s12 m4 l4">
+            <h4 class="grey-text">Fixed Deposits</h4>
             <p>This is a product that is designed for a WIN-WIN situtation for us and depositors. Members are free to fixed
                 their monies with us for a period of at least six (6) months at a pecentage flat rate that is determined
                 by the exco at intervals.</p>
 
         </div>
     </div>
-    <div class="row text-center">
-        <div class="col-sm-12">
-            <p>
-                <a href="/products" class="btn">Learn More
-                    <i class="large material-icons">mode_edit</i>
-            </a></p>
+
+    <div class="row center-align">
+        <div class="col s12">
+            <a href="/products" class="btn-large waves-effect waves-light"><i class="large material-icons left">label</i>Learn More
+                
+            </a>
         </div>
     </div>
 
     <div class="row">
-        <div class="col-md-7">
-            <h1 class="display-1">Built with Elegance</h1>
-            <h3 class="text-muted">...built for you and your lifestyle</h3>
+        <div class="col s12 m5 l5">
+            <h1>Built with Elegance</h1>
+            <h3 class="grey-text">...built for you and your lifestyle</h3>
             <p>
                 At the heart of our products and services lies a deep understanding of our customers and cooperators' behaviour, their demographics
                 and changing social lifestyle. That is why we follow you wherever your lifestyle may take you, now is the
                 time for the big idea whose time has come.
             </p>
         </div>
-        <div class="col-md-5">
-            <img src="{{asset('images/responsive.png')}}" class="img-fluid" />
+        <div class="col s12 m7  l7">
+            <img src="{{asset('images/responsive.png')}}" class="responsive-img" />
         </div>
     </div>
 @endsection

eda87f4911dc12713adef743be0e529c28cf2a27
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-17 09:50:02 +0100

    changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,30 +9,30 @@ class HomeController extends Controller
     //
     public function index (){
         $title ="MIDAS Home";
-        return view('Home.home')->with('title', $title);
+        return view('Home.home')->with('title',$title);
     }
     //about method
     public function about (){
         $title ="About MIDAS";
-        return view('Home.about')->with('title', $title);
+        return view('Home.about')->with('title',$title);
     }
 
     //Steering Committtee
     public function committee (){
         $title ="Steering Committee";
-        return view('Home.committee')->with('title', $title);
+        return view('Home.committee')->with('title',$title);
     }
 
     //Board
     public function board (){
         $title ="Board Members";
-        return view('Home.board')->with('title', $title);
+        return view('Home.board')->with('title',$title);
     }
 
     //products
     public function products (){
         $title = "Our Products";
-        return view('Home.products')->with('title', $title);
+        return view('Home.products')->with('title',$title);
     }
 
 }

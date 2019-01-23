23506fcdc63ef09d10bd6ac515680f964aec97fc
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-23 10:02:15 +0100

    Added index action method

--- app/Http/Controllers/MadminController.php
+++ app/Http/Controllers/MadminController.php
@@ -7,4 +7,31 @@ use Illuminate\Http\Request;
 class MadminController extends Controller
 {
     //
+    public function index (){
+        $title ="Dashboard Home";
+        return view('Madmin.index')->with('title',$title);
+    }
+    //about method
+    // public function about (){
+    //     $title ="About MIDAS";
+    //     return view('Home.about')->with('title',$title);
+    // }
+
+    //Steering Committtee
+    // public function committee (){
+    //     $title ="Steering Committee";
+    //     return view('Home.committee')->with('title',$title);
+    // }
+
+    //Board
+    // public function board (){
+    //     $title ="Board Members";
+    //     return view('Home.board')->with('title',$title);
+    // }
+
+    //products
+    // public function products (){
+    //     $title = "Our Products";
+    //     return view('Home.products')->with('title',$title);
+    // }
 }

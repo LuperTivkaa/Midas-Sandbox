da43303675f8dab4126ea308cdc695665c40a8b0
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-26 15:41:31 +0100

    created layout for madmin

--- resources/views/Madmin/index.blade.php
+++ resources/views/Madmin/index.blade.php
@@ -55,36 +55,38 @@
                 <h5>Main Content Goes Here</h5>
 
 
-                </div>
             </div>
         </div>
         <div class="col s12 m6 l4">
             <div class="side-content grey lighten-1">
                 <h5>Loan Applications Summary</h5>
-                <ul class="collection latest-comments">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas error eligendi illum mollitia, eaque minus
+                    ullam numquam eius ea sapiente sunt explicabo doloremque praesentium ratione dolorum pariatur eveniet
+                    natus iusto.</p>
+            </div>
+            <div class="side-content grey lighten-1">
+                <h5>Target Savings Summary</h5>
+                <ul class="collection with-header latest-comments">
                     <li class="collection-item avatar">
-                        <img src="{{asset('images/ternenge.jpg')}}" alt="" class="circle">
-                        <span class="title">John Ityo</span>
+                        <img src="{{asset('images/girl.png')}}" alt="" class="circle">
+                        <span class="title">Priscilla</span>
+                        <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio delectus
+                            quidem nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!</p>
+                        <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
                     </li>
                 </ul>
             </div>
-            <div>
-                <h5>Target Savings Summary</h5>
-                <ul class="collection with-header latest-comments">
-                        <li class="collection-item avatar">
-                            <img src="{{asset('images/ternenge.jpg')}}" alt="" class="circle">
-                            <span class="title">John Ityo</span>
-                        </li>
-                    </ul>
-            </div>
-            <div>
+            <div class="side-content grey lighten-1">
                 <h5>Top Loan Applicants</h5>
                 <ul class="collection with-header latest-comments">
-                        <li class="collection-item avatar">
-                            <img src="{{asset('images/ternenge.jpg')}}" alt="" class="circle">
-                            <span class="title">John Ityo</span>
-                        </li>
-                    </ul>
+                    <li class="collection-item avatar">
+                        <img src="{{asset('images/andy.jpg')}}" alt="" class="circle">
+                        <span class="title">Andy Shimakaa</span>
+                        <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio delectus
+                            quidem nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!</p>
+                        <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
+                    </li>
+                </ul>
             </div>
 
 

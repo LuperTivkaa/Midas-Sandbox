588b049b27d8aa154045ebddbf6d54b5205c98c0
Tivkaa Luper <tivkaaluper@gmail.com>
2019-01-31 20:55:49 +0100

    Side content container for includes

---
+++ resources/views/inc/admin-side-section.blade.php
@@ -0,0 +1,33 @@
+<div class="col s12 m6 l4">
+    <div class="side-content white">
+        <h5>Loan Applications Summary</h5>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas error eligendi illum mollitia, eaque minus ullam
+            numquam eius ea sapiente sunt explicabo doloremque praesentium ratione dolorum pariatur eveniet natus iusto.</p>
+    </div>
+    <div class="side-content white">
+        <h5>Target Savings Summary</h5>
+        <ul class="collection with-header latest-comments">
+            <li class="collection-item avatar">
+                <img src="{{asset('images/girl.png')}}" alt="" class="circle">
+                <span class="title">Priscilla</span>
+                <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio delectus quidem
+                    nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!
+                </p>
+                <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
+            </li>
+        </ul>
+    </div>
+    <div class="side-content white">
+        <h5>Top Loan Applicants</h5>
+        <ul class="collection with-header latest-comments">
+            <li class="collection-item avatar">
+                <img src="{{asset('images/andy.jpg')}}" alt="" class="circle">
+                <span class="title">Andy Shimakaa</span>
+                <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio delectus quidem
+                    nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!
+                </p>
+                <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
+            </li>
+        </ul>
+    </div>
+</div>

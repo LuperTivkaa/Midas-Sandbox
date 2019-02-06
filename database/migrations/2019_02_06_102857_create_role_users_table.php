684c35d51bd6fa60082893e0bdce6753674edbc3
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-06 12:26:23 +0100

    role user migrations

--- database/migrations/2019_02_06_102857_create_role_users_table.php
+++ database/migrations/2019_02_06_102857_create_role_users_table.php
@@ -15,8 +15,13 @@ class CreateRoleUsersTable extends Migration
     {
         Schema::create('role_users', function (Blueprint $table) {
             $table->increments('id');
-            
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('role_id');
             $table->timestamps();
+
+            $table->unique(['user_id','role_id']);
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
         });
     }
 

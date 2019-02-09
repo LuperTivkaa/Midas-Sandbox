a2758f3045e21f28edbb2eb6562a43e296290667
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 15:18:58 +0100

    created store method

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,17 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class RegistrationController extends Controller
 {
     //Register  new staff
     public function createStaff (){
-        $title ="New Staff";
+        $title ="New User";
         return view('Registration.createStaff')->with('title',$title);
     }
 
-    public function storeStaff (){
-        $title ="New Staff";
-        return view('Registration.createStaff')->with('title',$title);
-    }
+   //Store  new staff
+   public function storeUser (){
+    //validate the form
+    $this->validate(request(), [
+        'payment_number'=>'required',
+        'password' =>'required',
+        'email' =>'required|email',
+
+    ]);
+
+    //Create the staff
+    $user = User::create(request(['payment_number','password','email']));
+
+
+    //Login the User
+    //*** */auth()->login($staff);
+    //flash message
+    //*** */session()->flash('message','Staff Created');
+    //redirect to route
+    //*** */return redirect('')->home;
+// ***return redirect('/Staff/New')->with('success','Staff Created');
+//return redirect('/Staff/New');
+     return 13234;
+}
 }

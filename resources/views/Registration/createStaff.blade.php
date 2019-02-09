3768401924dfb3479b46f1a82f6c5b6b946a3321
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 14:50:09 +0100

    Create Staff form

--- resources/views/Registration/createStaff.blade.php
+++ resources/views/Registration/createStaff.blade.php
@@ -1,6 +1,42 @@
-@extends('Layouts.admin.app') 
+@extends('Layouts.admin-app') 
 @section('main-content')
-<form method="POST" action="/profile">
-    @csrf ...
-</form>
+<div class="container">
+    <div class="row">
+        <div class="col s3">
+            <h5 class="teal-text">New User</h5>
+        </div>
+
+        <form class="col s12" method="POST" action="/profile">
+            @csrf
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="payment_number" name="payment_number" type="text" class="validate">
+                    <label for="payment_number">Payment Number</label>
+                </div>
+
+
+                <div class="input-field col s6">
+                    <input id="password" name="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                </div>
+            </div>
+
+            {{--
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="password" name="password" type="password" class="validate">
+                    <label for="password">Password</label>
+                </div>
+            </div> --}}
+            <div class="row">
+                <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">Email</label>
+                </div>
+            </div>
+
+            <button type="submit" class="btn">Create</button>
+        </form>
+    </div>
+</div>
 @endsection

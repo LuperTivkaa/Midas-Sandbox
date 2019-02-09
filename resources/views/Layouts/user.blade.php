53065f94a436e2d87734a87edaabe88c5b96ff40
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 14:49:02 +0100

    User Dashboard

---
+++ resources/views/Layouts/user.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?famaily=Open+Sans:300,400,600">
+    <link rel="stylesheet" href="{{asset('css/portal.css')}}">
+    <title>MIDAS- User Dashboard:: {{$title}}</title>
+</head>
+
+<body>
+
+    @yield('admin')
+    <script src="{{asset('js/app.js')}}"></script>
+</body>
+
+</html>

fa042f22e64f2c99011e03db90bfea19687fec86
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 14:44:48 +0100

    Created relationship

--- app/User.php
+++ app/User.php
@@ -26,4 +26,9 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    //Define relationship with roles
+    public function roles(){
+        return $this->belongsToMany(Role::class,'role_users');
+    }
 }

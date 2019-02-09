96c1e15034242a0d5abc75a15e1f24e5462eb012
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 14:44:02 +0100

    Created relationship

--- app/Role.php
+++ app/Role.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model
 {
     //
+    protected $fillable = [
+        'name',
+        'description', 
+        'permissions',
+        
+   ];
+//Define relationships with Users
+public function users(){
+    return $this->belongsToMany(User::class,'role_users');
+
+}
 }

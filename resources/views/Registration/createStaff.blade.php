d5b04958c26fea53209e2c2b4a826d941eb900c0
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-09 18:38:09 +0100

    Included csrf field and messages section

--- resources/views/Registration/createStaff.blade.php
+++ resources/views/Registration/createStaff.blade.php
@@ -1,13 +1,14 @@
 @extends('Layouts.admin-app') 
 @section('main-content')
 <div class="container">
+    @include('inc.messages')
     <div class="row">
         <div class="col s3">
             <h5 class="teal-text">New User</h5>
         </div>
 
-        <form class="col s12" method="POST" action="/User/New">
-            @csrf
+        <form class="col s12" method="POST" action="/User/Create">
+            {{ csrf_field() }}
             <div class="row">
                 <div class="input-field col s6">
                     <input id="payment_number" name="payment_number" type="text" class="validate">
@@ -30,7 +31,7 @@
             </div> --}}
             <div class="row">
                 <div class="input-field col s12">
-                    <input id="email" type="email" class="validate">
+                    <input id="email" name="email" type="email" class="validate">
                     <label for="email">Email</label>
                 </div>
             </div>

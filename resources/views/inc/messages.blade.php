524ddb2c69a4081d14c7a5eecbf7e8d36b861ce4
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-15 02:47:17 +0100

    refactored messages partial

--- resources/views/inc/messages.blade.php
+++ resources/views/inc/messages.blade.php
@@ -1,20 +1,17 @@
 <div class="row">
-    <div class="container">
+    <div class="col s12">
         @if(count($errors)>0) @foreach ($errors->all() as $error)
-        <div class="col s12 red lighten-3 section">
-            {{$error}}
-        </div>
-        @endforeach @endif {{-- @if (session('success'))
-        <div class="col s12 green">{{session('success')}}</div>
-        @endif --}} @if (session('error'))
-        <div class="col s12 red lighten-3">
-            {{session('error')}}
-        </div>
-        @endif
+        <p class="red-text lighten-3">{{$error}}</p>
+        @endforeach @endif
     </div>
+    {{-- @if (session('success'))
+    <div class="col s12 green">{{session('success')}}</div>
+    @endif --}} {{-- @if (session('error'))
+    <div class="col s12 red lighten-3">
+        {{session('error')}}
+    </div>
+    @endif --}}
 </div>
-
-
 
 @if ($flash = session('message'))
 <div id='f-message' class="col s12 green accent-3">{{$flash}}</div>

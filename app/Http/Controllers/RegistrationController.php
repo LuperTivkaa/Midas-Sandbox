dd50511badb449b9c6fd5bc1baea59c51c482a83
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-15 02:19:17 +0100

    Refactored registration controller

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -3,39 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
 use App\User;
+use App\Role;
 
 class RegistrationController extends Controller
 {
     //Register  new staff
     public function createStaff (){
         $title ="New User";
-        return view('Registration.createStaff')->with('title',$title);
+        $roles = Role::orderBy('name')->pluck('name','id');
+        return view('Registration.createStaff',compact('title','roles'));
     }
 
    //Store  new staff
-   public function storeUser (){
+   public function storeUser (Request $request){
     //validate the form
     $this->validate(request(), [
         'payment_number'=>'required',
-        'password' =>'required',
+        'password' =>'required|confirmed',
         'email' =>'required|email',
-
     ]);
 
+    $user = new User();
+    $user->payment_number = $request['payment_number'];
+    $user->password = Hash::make($request['password']);
+    $user->email = $request['email'];
+    $user->save();
     //Create the staff
-    $user = User::create(request(['payment_number','password','email']));
-
+   // $user = User::create(request(['payment_number','password','email']));
+    $user->roles()->attach(request(['role']));
 
     //Login the User
-    //*** */auth()->login($staff);
+    auth()->login($user);
     //flash message
     //*** */session()->flash('message','Staff Created');
     //redirect to route
     //*** */return redirect('')->home;
 // ***return redirect('/Staff/New')->with('success','Staff Created');
-//return redirect('/Staff/New');
-
-     return 13234;
+return redirect('/New');
 }
 }

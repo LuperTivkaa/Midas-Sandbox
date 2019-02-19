64a8584a3083dfdaf185dff78288505833cce5e6
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-19 15:39:55 +0100

    Cahnged layout design

--- resources/views/Registration/createStaff.blade.php
+++ resources/views/Registration/createStaff.blade.php
@@ -10,7 +10,12 @@
         <form class="col s12" method="POST" action="/Create">
             {{ csrf_field() }}
             <div class="row">
-                <div class="input-field col s12">
+                <div class="input-field col s6">
+                    <input id="staff_no" name="staff_no" type="text" class="validate" required>
+                    <label for="staff_no">ID Card Number</label>
+                </div>
+
+                <div class="input-field col s6">
                     <input id="payment_number" name="payment_number" type="text" class="validate" required>
                     <label for="payment_number">Payment Number</label>
                 </div>
@@ -37,12 +42,97 @@
 
                 <div class="input-field col s6">
                     <select id="role" name="role">
-                        <option value="" disabled selected>Choose your role</option>
+                        <option value="" disabled>System Role</option>
                         @foreach ($roles as $id=>$role)
                         <option value="{{$id}}">{{$role}}</option>
                         @endforeach
                     </select>
                     <label>System Role</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="input-field col s4">
+                    <select id="title" name="title">
+                                <option value="Mr">Mr</option>
+                                <option value="Mrs">Mrs</option>
+                                <option value="Miss">Miss</option>
+                                <option value="Dr">Dr</option>
+                            </select>
+                    <label>Title</label>
+                </div>
+
+                <div class="input-field col s4">
+                    <select id="sex" name="sex">
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
+                    <label>Gender</label>
+                </div>
+                <div class="input-field col s4">
+                    <select id="marital_status" name="marital_status">
+                                <option value="Married">Married</option>
+                                <option value="Single">Single</option>
+                                <option value="Divorced">Divorced</option>
+                                <option value="Widow">Widow</option>
+                                <option value="Widower">Widower</option>
+                            </select>
+                    <label>Marital Status</label>
+                </div>
+            </div>
+
+            <div class="row">
+
+
+                <div class="input-field col s4">
+                    <input id="first_name" name="first_name" type="text" class="validate" required>
+                    <label for="first_name">Surname</label>
+                </div>
+
+                <div class="input-field col s4">
+                    <input id="last_name" name="last_name" type="text" class="validate" required>
+                    <label for="last_name">Last name</label>
+                </div>
+                <div class="input-field col s4">
+                    <input id="other_name" name="other_name" type="text" class="validate" required>
+                    <label for="other_name">Other name</label>
+                </div>
+
+            </div>
+
+            <div class="row">
+
+                <div class="input-field col s3">
+                    <select id="employ_type" name="employ_type">
+                                <option value="Permanent">Permanent</option>
+                                <option value="Temporal">Temporal</option>
+                    </select>
+                    <label>Employment Type</label>
+                </div>
+
+                <div class="input-field col s3">
+                    <input id="dept" name="dept" type="text" class="validate" required>
+                    <label for="dept">Dept</label>
+                </div>
+
+                <div class="input-field col s3">
+                    <input id="phone" name="phone" type="text" class="validate" required>
+                    <label for="phone">Phone</label>
+                </div>
+                <div class="input-field col s3">
+                    <input id="dob" name="dob" type="text" class="validate datepicker" required>
+                    <label for="dob">DOB</label>
+                </div>
+
+            </div>
+
+            <div class="row">
+                <div class="input-field col s6">
+                    <input id="home_add" name="home_add" type="text" class="validate" required>
+                    <label for="home_add">Home Address</label>
+                </div>
+                <div class="input-field col s6">
+                    <input id="res_add" name="res_add" type="text" class="validate" required>
+                    <label for="res_add">Residential Address</label>
                 </div>
             </div>
 

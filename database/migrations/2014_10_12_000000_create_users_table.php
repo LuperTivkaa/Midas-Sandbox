ba37c295cb42a1ea5fc7c1c4a8c3d66d8d5b5fb4
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-19 15:36:51 +0100

    added properties on user model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('username')->default('midastouch');
+           // $table->string('username')->default('midastouch');
             $table->integer('payment_number')->unique();
-            $table->string('password');
+            $table->string('password')->unique();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('title');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('other_name')->nullable();
+            $table->string('sex');
+            $table->integer('staff_no');
+            $table->string('dept');
+            $table->string('phone');
+            $table->string('marital_status');
+            $table->string('home_add');
+            $table->string('res_add');
+            $table->date('dob');
+            $table->string('employ_type');
+            $table->string('job_cadre');
             $table->rememberToken();
             $table->timestamps();
         });

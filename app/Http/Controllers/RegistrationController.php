e3cf8a84276a7590d406af9a13f86f62e39497e0
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-20 09:29:23 +0100

    Edited store methods

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -30,6 +30,20 @@ class RegistrationController extends Controller
     $user->payment_number = $request['payment_number'];
     $user->password = Hash::make($request['password']);
     $user->email = $request['email'];
+    $user->title = $request['title'];
+    $user->first_name = $request['first_name'];
+    $user->last_name = $request['last_name'];
+    $user->other_name = $request['other_name'];
+    $user->employ_type = $request['employ_type'];
+    $user->dept = $request['dept'];
+    $user->phone = $request['phone'];
+    $user->dob = $request['dob'];
+    $user->home_add = $request['home_add'];
+    $user->rse_add = $request['res_add'];
+    $user->sex = $request['sex'];
+    $user->job_cadre = $request['job_cadre'];
+    $user->staff_no = $request['staff_no'];
+    $user->marital_status = $request['marital_status'];
     $user->save();
     //Create the staff
    // $user = User::create(request(['payment_number','password','email']));
@@ -38,7 +52,7 @@ class RegistrationController extends Controller
     //Login the User
     //auth()->login($user);
     //flash message
-    //*** */session()->flash('message','Staff Created');
+    session()->flash('message','Staff Created');
     //redirect to route
     //*** */return redirect('')->home;
 // ***return redirect('/Staff/New')->with('success','Staff Created');
@@ -51,6 +65,48 @@ public function nextOfKin (){
     //$roles = Role::orderBy('name')->pluck('name','id');
     return view('Registration.nok',compact('title'));
 }
+
+//Save next of kin
+public function nokStore (){
+    //validate the form
+    $this->validate(request(), [
+        'payment_number'=>'required',
+        'password' =>'required|confirmed',
+        'email' =>'required|email',
+    ]);
+
+    $user = new User();
+    $user->payment_number = $request['payment_number'];
+    $user->password = Hash::make($request['password']);
+    $user->email = $request['email'];
+    $user->title = $request['title'];
+    $user->first_name = $request['first_name'];
+    $user->last_name = $request['last_name'];
+    $user->other_name = $request['other_name'];
+    $user->employ_type = $request['employ_type'];
+    $user->dept = $request['dept'];
+    $user->phone = $request['phone'];
+    $user->dob = $request['dob'];
+    $user->home_add = $request['home_add'];
+    $user->rse_add = $request['res_add'];
+    $user->sex = $request['sex'];
+    $user->job_cadre = $request['job_cadre'];
+    $user->staff_no = $request['staff_no'];
+    $user->marital_status = $request['marital_status'];
+    $user->save();
+    //Create the staff
+   
+    $user->roles()->attach(request(['role']));
+
+  
+    //flash message
+    session()->flash('message','Staff Created');
+    //redirect to route
+    //*** */return redirect('')->home;
+// ***return redirect('/Staff/New')->with('success','Staff Created');
+return redirect('/New');
+}
+
 
 //form for nok 
 public function bank(){

926c56feaa0892efd45792c3d51861babbb23dc4
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-23 17:56:40 +0100

    removed space

--- resources/views/Layouts/admin-app.blade.php
+++ resources/views/Layouts/admin-app.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <link href="{{asset('fontawesome-assets/css/fontawesome.css')}}" rel="stylesheet">
     <link href="{{asset('fontawesome-assets/css/brands.css')}}" rel="stylesheet">
     <link href="{{asset('fontawesome-assets/css/solid.css')}}" rel="stylesheet">

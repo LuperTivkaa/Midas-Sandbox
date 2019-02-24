f5c8d96b09afbdd27d7c345110bb833b250b4c8e
Tivkaa Luper <tivkaaluper@gmail.com>
2019-02-24 15:38:25 +0100

    Added table for display of list

--- resources/views/Users/listUsers.blade.php
+++ resources/views/Users/listUsers.blade.php
@@ -3,14 +3,51 @@
 <div class="container">
     @include('inc.messages')
     <div class="row">
-        <div class="col s12">
+        <div class="col 12">
             <h5 class="teal-text">All Users</h5>
             <div class="divider"></div>
+        </div>
+    </div>
+    <div class="row">
+
+        <div class="col s3">
+            <a href="" class="btn">New</a>
+        </div>
+        <div class="col s6">
+
+            <a href="" class="btn">New</a>
+
+        </div>
+        <div class="col s3">
+            <a href="" class="btn">New</a>
+        </div>
+    </div>
+
+
+
+    <div class="row">
+        <div class="col s12">
             @if (count($users)>=1)
-            <ul>
-                @foreach ($users as $user)
-                <li>{{$user->first_name}}</li>
-                @endforeach
+            <table class="highlight">
+                <thead>
+                    <tr>
+                        <th>Surname</th>
+                        <th>Last Name</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                    <tr>
+                        <td><a href="/userDetails/{{$user->id}}">{{$user->first_name}}</a></td>
+                        <td>{{$user->last_name}}</td>
+                        <td>Active</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+            <ul class="pagination">
+                {{$users->links()}}
             </ul>
             @else
             <p>No Users Created Yet</p>

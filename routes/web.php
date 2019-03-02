7d46f71981072e75d556d003ce11c1a469e935d9
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-02 23:05:26 +0100

    New user management routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,14 @@
 //     return view('welcome');
 // });
 
+  //
+    // Route::middleware(['auth'])->group(function () {
+    //     Route::get('leadsadd','crmcontroller@addleads');
+    //     Route::get('leadslist', 'crmcontroller@leadslist');
+    //     Route::any('leadview/{id}','crmcontroller@show');
+    //     Route::get('leadedit/{id}','crmcontroller@edit');
+    // });
+
 //ROUTES FOR HOME CONTROLLER
 Route::get('/', 'HomeController@index');
 Route::get('/about', 'HomeController@about');
@@ -26,21 +34,21 @@ Route::get('/gallery', 'HomeController@gallery');
 
 //ROUTES FOR ADMIN DASHBOARD
 
-Route::get('/Dashboard', 'MadminController@index')->middleware('auth');
-
-
+Route::get('/Dashboard', 'MadminController@index');
 //dashobard routes
-Route::get('/Dashboard/home','DashboardController@index')->middleware('auth');
+Route::get('/Dashboard/home','DashboardController@index');
 
 //REGISTRATION ROUTES
+    Route::middleware(['auth'])->group(function () {
+        
+        Route::get('/New','RegistrationController@createUser');
+        Route::post('/Create','RegistrationController@storeUser');
+        Route::get('/Nok','RegistrationController@nextOfKin');
+        Route::post('/nokStore','RegistrationController@nokStore');
+        Route::get('/bank','RegistrationController@bank');
+        Route::post('/bankStore','RegistrationController@bankStore');
 
-Route::get('/New','RegistrationController@createUser');
-Route::post('/Create','RegistrationController@storeUser');
-Route::get('/Nok','RegistrationController@nextOfKin');
-Route::post('/nokStore','RegistrationController@nokStore');
-Route::get('/bank','RegistrationController@bank');
-Route::post('/bankStore','RegistrationController@bankStore');
-
+    });
 
 // Route::group(['prefix'=>'User'], function(){
 //     Route::get('/New','RegistrationController@createStaff')
@@ -55,4 +63,13 @@ Route::post('/signin','SessionController@store');
 Route::get('/logout','SessionController@destroy');
 
 //Manage Users  Controller
-Route::get('user/all','UsersController@index');
+Route::get('/user/all','UsersController@index');
+Route::get('/user/bank','UsersController@bankList');
+Route::get('/user/nok','UsersController@nokList');
+Route::get('/userDetails/{id}','UsersController@profileDetails');
+Route::get('/editProfile/{id}','UsersController@editProfile');
+Route::post('/updateProfile/{id}','UsersController@updateProfile');
+Route::get('/editBank/{id}','UsersController@editBank');
+Route::post('/updateBank/{id}','UsersController@updateBank');
+Route::get('/editNok/{id}','UsersController@editNok');
+Route::post('/updateNok/{id}','UsersController@updateNok');

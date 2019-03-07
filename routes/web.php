96f934cf1316e9141016ec4e8bb81c859be11531
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-07 13:02:15 +0100

    Edited routes

--- routes/web.php
+++ routes/web.php
@@ -40,7 +40,6 @@ Route::get('/Dashboard/home','DashboardController@index');
 
 //REGISTRATION ROUTES
     Route::middleware(['auth'])->group(function () {
-        
         Route::get('/New','RegistrationController@createUser');
         Route::post('/Create','RegistrationController@storeUser');
         Route::get('/Nok','RegistrationController@nextOfKin');
@@ -49,6 +48,7 @@ Route::get('/Dashboard/home','DashboardController@index');
         Route::post('/bankStore','RegistrationController@bankStore');
 
     });
+    
 
 //Session/login controller
 Route::get('/login', 'SessionController@create')->name('login');
@@ -56,6 +56,7 @@ Route::post('/signin','SessionController@store');
 Route::get('/logout','SessionController@destroy');
 
 //Manage Users  Controller
+Route::middleware(['auth'])->group(function () {
 Route::get('/user/all','UsersController@index');
 Route::get('/user/bank','UsersController@bankList');
 Route::get('/user/nok','UsersController@nokList');
@@ -66,10 +67,17 @@ Route::get('/editBank/{id}','UsersController@editBank');
 Route::post('/updateBank/{id}','UsersController@updateBank');
 Route::get('/editNok/{id}','UsersController@editNok');
 Route::post('/updateNok/{id}','UsersController@updateNok');
+});
 
 //Products routes
 Route::get('/products','ProductsController@index');
 Route::get('/product/create','ProductsController@create');
 Route::post('/product/store','ProductsController@store');
+Route::get('/product/detail/{id}','ProductsController@show');
 Route::get('/editProduct/{id}','ProductsController@edit');
 Route::post('/updateProduct/{id}','ProductsController@update');
+
+//Product Subscription
+Route::get('/subscriptions','ProductSubscriptionController@index');
+Route::get('/new_subscription','ProductSubscriptionController@create');
+

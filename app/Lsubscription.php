9d3ea408e6bcf116d0a0b139b50f84e2f7e46b30
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-17 17:59:46 +0100

    Loan subscription model

---
+++ app/Lsubscription.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Lsubscription extends Model
+{
+    //
+      //relationship with user
+      public function user(){
+        return $this->belongsTo(User::class);
+        }
+    
+        //loan relationship
+        //A subscription belongs to a loan
+        public function loan(){
+            return $this->belongsTo(Loan::class);
+        }
+    
+        protected $dates = ['created_at', 'updated_at','loan_start_date','loan_end_date'];
+}

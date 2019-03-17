e8a94473d85ec710aa7115b4215991347407a0bb
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-17 18:24:24 +0100

    New product subscription form

--- resources/views/ProductSub/create.blade.php
+++ resources/views/ProductSub/create.blade.php
@@ -6,51 +6,44 @@
     <div class="row">
         <div class="col s12 subject-header">
             <span class="teal-text">New  Product Subscription</span>
-            <span><a href="/subscriptions"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Subscription">view_list</i></a></span>            {{-- <span><a href="/New"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Create User">person_add</i></a></span>
-            <span><a href=""><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="User's Savings">account_balance_wallet</i></a></span>
-            <span><a href=""><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="User's Target Savings">monetization_on</i></a></span>
-            <span><a href=""><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Subscribed Products Schemes">local_grocery_store</i></a></span>            --}}
+            <span><a href="/subscriptions"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Subscription">view_list</i></a></span>
+            <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Products">apps</i></a></span>
         </div>
     </div>
     <div class="row">
-        <form class="col s12" method="POST" action="/Product-sub/create">
+        <form class="col s12" method="POST" action="/productsub">
             {{ csrf_field() }}
             <div class="row">
-                <div class="input-field col s12">
-                    <input id="payment_id" name="payment_id" type="text" class="validate" required>
+                <div class="input-field col s12 m6 l6">
+                    <input placeholder="IPPIS or GFMIS" id="payment_id" name="payment_id" type="text" class="validate">
                     <label for="payment_id">Payment ID</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s6">
+                <div class="input-field col s12 m6 l6">
                     <select id="product" name="product">
-                                {{-- <option value="" disabled>Choose role</option> --}}
-                                <option value="" disabled>Product</option>
-                                {{-- @foreach ($products as $id=>$name)
-                                <option value="{{$id}}">{{$name}}</option>
-                                @endforeach --}}
-                            </select>
+                        {{-- <option value="" disabled>Choose role</option> --}}
+                        @foreach ($prodList as $id=>$name)
+                        <option value="{{$id}}">{{$name}}</option>
+                        @endforeach
+                    </select>
                     <label>Product</label>
                 </div>
-
-                <div class="input-field col s6">
-                    <input id="units" name="units" type="text" class="validate" required>
+                <div class="input-field col s12 m6 l6">
+                    <input id="units" name="units" type="text" class="validate">
                     <label for="units">Units</label>
                 </div>
             </div>
-
             <div class="row">
-                <div class="input-field col s6">
-                    <input id="start_date" name="start_date" type="text" class="validate datepicker" required>
+                <div class="input-field col s12 m6 l6">
+                    <input id="start_date" name="start_date" type="text" class="validate datepicker">
                     <label for="start_date">Start Date</label>
                 </div>
-
-                <div class="input-field col s6">
-                    <input id="end_start" name="end_start" type="text" class="validate datepicker" required>
-                    <label for="end_start">End Date</label>
+                <div class="input-field col s12 m6 l6">
+                    <input id="end_date" name="end_date" type="text" class="validate datepicker">
+                    <label for="end_date">End Date</label>
                 </div>
             </div>
-
             <button type="submit" class="btn">Subscribe</button>
         </form>
     </div>

a8efca1bab5661b961d3333e1f4b0ef0feaabd3c
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-17 18:14:34 +0100

    target savings migration

---
+++ database/migrations/2019_03_11_093600_create_targetsavings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTargetsavingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('targetsavings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->integer('created_by'); //ID of the logged in staff
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('targetsavings');
+    }
+}

4c1a7ebd0f697d2de331e090c64faff597083a35
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-17 18:01:57 +0100

    Product subscription model

---
+++ app/Psubscription.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Psubscription extends Model
+{
+    
+    //relationship with user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    //relationship with products
+    //Done ; A product subscription belonngs to a product
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    protected $dates = ['created_at', 'updated_at','start_date','end_date'];
+}

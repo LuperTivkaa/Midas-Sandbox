e35b03b14764246c40be766ac83aea85f529aa14
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-17 18:00:28 +0100

    modeified relationship details

--- app/Loan.php
+++ app/Loan.php
@@ -8,7 +8,8 @@ class Loan extends Model
 {
     //
     //relationship with products
-    public function loansubscription(){
-        return $this->hasMany(LoanSubscription::class);
+    //A particular loan can have more than one subscriptions
+    public function loansubscriptions(){
+        return $this->hasMany(Lsubscription::class);
     }
 }

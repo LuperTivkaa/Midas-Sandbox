80e4ae49dcaa65b9529c750c83a8aaf4e4d77d7c
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-20 13:58:36 +0100

    Added relationship with user

--- app/Targetsr.php
+++ app/Targetsr.php
@@ -7,5 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Targetsr extends Model
 {
     //
+     //Each saving belongs to a user
+     public function user(){
+        return $this->belongsTo(User::class);
+    }
     protected $dates = ['created_at', 'updated_at','review_date'];
 }

ae15830d393a6e4ffca7ecb4922be98ad3884ff6
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-20 13:24:50 +0100

    saings review migration

---
+++ database/migrations/2019_03_20_110631_create_savings_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSavingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('savings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->decimal('amount_saved',8,2);
+            $table->date('entry_for');
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('savings');
+    }
+}

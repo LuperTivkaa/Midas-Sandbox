8d4fa80c77090319e5a1403eb0e6ce7c17e80668
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-20 13:56:45 +0100

    Added user relationship

--- app/Savingreview.php
+++ app/Savingreview.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Savingreview extends Model
 {
     //
+    //Each saving review belongs to a user
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
 }

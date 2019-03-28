2b13a625bbb87aaf949122018aca55ccaa8ce717
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-28 15:09:01 +0100

    Added user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,7 +15,21 @@ use Faker\Generator as Faker;
 
 $factory->define(App\User::class, function (Faker $faker) {
     return [
-        'name' => $faker->name,
+        'payment_number' => $faker->unique()->randomNumber($nbDigits = NULL, $strict = false),
+        'title' => $faker->title,
+        'first_name' => $faker->firstName,
+        'last_name' => $faker->lastName,
+        'other_name' => $faker->optional()->lastName,
+        'sex' => $faker->randomElement(['male', 'female']),
+        'staff_no' => $faker->numberBetween($min = 1, $max = 9000),
+        'dept' => $faker->randomElement(['Administration', 'Finance','Records','Procurement','Nursing','O&G']),
+        'phone' => $faker->phoneNumber,
+        'marital_status' => $faker->randomElement(['Married', 'Single','Widower','Divorce','Window']),
+        'home_add' => $faker->address,
+        'res_add' => $faker->address,
+        'dob' => $faker->date($format = 'Y-m-d', $max = 'now'),
+        'employ_type' => $faker->randomElement(['Permanent', 'Contract']),
+        'job_cadre' => $faker->randomElement(['Junior Staff', 'Senior Staff']),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret

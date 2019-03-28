682df92dcc3317bdbf9ea92bbffc3cc2c7f7649a
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-28 15:14:21 +0100

    Edited database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(RoleTableSeeder::class);
+        $this->call([
+            ProductSeeder::class,
+            LoanProductSeeder::class,
+            RoleTableSeeder::class,
+            UserSeeder::class,    
+        ]);
     }
 }

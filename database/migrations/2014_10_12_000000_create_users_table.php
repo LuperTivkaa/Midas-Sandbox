0f18ed4b95cad6478bc9eaa496dfb736effe1b97
Tivkaa Luper <tivkaaluper@gmail.com>
2019-03-28 15:09:31 +0100

    Changed migraion rule on password

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->bigInteger('payment_number')->unique();
-            $table->string('password')->unique();
+            $table->string('password');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('title');

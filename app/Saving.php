744f3300bdd1acc3292293f512825403a2a01a2e
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-10 11:38:08 +0100

    Added fillable properties

--- app/Saving.php
+++ app/Saving.php
@@ -7,6 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 class Saving extends Model
 {
     //
+
+    protected $fillable = [
+        'user_id', 
+        'amount_saved', 
+        'entry_date',
+        'created_by',
+    ];
+
+    protected $dates = ['created_at', 'updated_at','entry_date'];
+
     //Each saving belongs to a user
     public function user(){
         return $this->belongsTo(User::class);

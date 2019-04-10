66e988845a99872e1774730057f45f78aeadd973
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-10 11:38:39 +0100

    Added fillable properties

--- app/TargetSaving.php
+++ app/TargetSaving.php
@@ -7,6 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Targetsaving extends Model
 {
     //
+    protected $fillable = [
+      'user_id', 
+      'amount', 
+      'entry_date',
+      'created_by',
+  ];
       //Each saving belongs to a user
       public function user(){
         return $this->belongsTo(User::class);

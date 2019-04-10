83737c71370c276ce6ab3be8fe225c7bf7ab04ae
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-10 12:12:15 +0100

    Monthly saving upload

---
+++ resources/views/MonthlySaving/savingsUpload.blade.php
@@ -0,0 +1,35 @@
+@extends('Layouts.admin-app') 
+@section('main-content')
+<div class="container">
+    {{--
+    @include('inc.messages') --}}
+    <div class="row">
+        <div class="col s12 subject-header">
+            <span class="teal-text">NEW SAVING UPLOAD</span>
+            <span><a href="/saving/search"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Search Savings">search</i></a></span>
+            <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Upload Savings">cloud_upload</i></a></span>
+            <span><a href="/"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All User Savings">view_list</i></a></span>
+            <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Savings">visibility</i></a></span>
+
+        </div>
+    </div>
+    <div class="row">
+        <form class="col s12" method="POST" action="{{route('savings.upload')}}" enctype="multipart/form-data">
+            {{ csrf_field() }}
+            <div class="row">
+
+                <div class="file-field input-field col s12 m6 l6">
+                    <div class="btn">
+                        <span>Browse</span>
+                        <input type="file" name="saving_import">
+                    </div>
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text" placeholder="Choose xlsx file">
+                    </div>
+                </div>
+            </div>
+            <button type="submit" class="btn">Upload Saving</button>
+        </form>
+    </div>
+</div>
+@endsection

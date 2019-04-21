4db7efa2a01db1fe3d16eb7e3d4da076176bf702
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-21 09:52:34 +0100

    Create index method

--- app/Http/Controllers/DeductionsController.php
+++ app/Http/Controllers/DeductionsController.php
@@ -7,4 +7,9 @@ use Illuminate\Http\Request;
 class DeductionsController extends Controller
 {
     //
+    //Master deductions for IPPIS upload
+    public function index(){
+        $title = 'Master IPPIS Deductions';
+        return view ('Deductions.master',compact('title'));
+    }
 }

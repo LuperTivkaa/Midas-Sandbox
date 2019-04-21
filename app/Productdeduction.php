1140f4f68db8f6e14bcd609581c90e1657aeea61
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-21 09:47:47 +0100

    Add product relationship

--- app/Productdeduction.php
+++ app/Productdeduction.php
@@ -3,24 +3,44 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
+use App\Savingreview;
+use App\Psubscription;
+use App\Lsubscription;
 class Productdeduction extends Model
 {
     //
     protected $fillable = [
         'user_id', 
-        'psubscription_id', 
+        'psubscription_id',
+        'product_id',
+        'monthly_deduction', 
         'entry_date',
         'uploaded_by',
     ];
 
     protected $dates = ['created_at', 'updated_at','entry_date'];
 
-    //Each saving belongs to a user
+    //Each product deduction belongs to a user
     public function user(){
         return $this->belongsTo(User::class);
     }
 
+
     //Each deduction belongs to a product subscription
-    //function stub here
+    // public function productsubscription(){
+    //     return $this->belongsTo(Psubscription::class);
+    // }
+
+    //Each product deduction belongs to a product
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
+
+    //Product deduction details
+    public static function deductionDetails($id){
+        return static::find($id);
+    } 
+
+   
 }

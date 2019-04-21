02e93d7f972d6d7c4a7fac06309383919adbebb9
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-21 07:31:25 +0100

    Added new user active and pending loans methods

--- app/Lsubscription.php
+++ app/Lsubscription.php
@@ -23,4 +23,26 @@ class Lsubscription extends Model
       }
     
         protected $dates = ['created_at', 'updated_at','loan_start_date','loan_end_date'];
+
+        
+        //User Active loans
+    public static function activeLoans($id){
+        return static::where('user_id',$id)
+        ->where(function ($query){
+            $query->where('loan_status','Active');
+        })->with(['loan' => function ($query) {
+        $query->orderBy('description', 'desc');
+        }])->get();
+      }
+      //User pending loans
+public static function pendingLoans($id){
+  return static::where('user_id',$id)
+        ->where(function ($query){
+            $query->where('loan_status','=','Pending');
+        })->with(['loan' => function ($query) {
+        $query->orderBy('description', 'desc');
+        }])->get();
+}
+
+      
 }

f86fa21554f924ef58800caef123d66ad1565fa8
Tivkaa Luper <tivkaaluper@gmail.com>
2019-04-21 09:48:53 +0100

    Add define class methods

--- app/Psubscription.php
+++ app/Psubscription.php
@@ -3,6 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
+use App\Savingreview;
+use App\Psubscription;
+use App\Lsubscription;
 
 class Psubscription extends Model
 {
@@ -18,5 +22,70 @@ class Psubscription extends Model
         return $this->belongsTo(Product::class);
     }
 
+  
+
     protected $dates = ['created_at', 'updated_at','start_date','end_date'];
+
+
+    //all product subscriptions
+    public static function allProductSubscriptions(){
+
+        // return  $this::where('status', 'Active')
+        // ->where(function ($query) {
+        //     $query->where('status', '=', 'Active');
+        // })->with(['user','product'])
+        // ->paginate(50);
+        return  static::where('status', 'Active')
+        ->with(['user','product'])
+        ->get();
+    }
+
+    // Product subscriptionn item details
+    public static function itemDetails($id){
+        return static::find($id);
+    } 
+
+     //User active product subscriptions
+     public static function userProducts($id){
+
+        return static::where('user_id', '=', $id)
+        ->where(function ($query) {
+            $query->where('status', '=', 'Active');
+        })->with(['product','user'])->orderBy('start_date','desc')
+        ->get();
+        
+    }
+
+    //User Pending product subscriptions
+     public static function pendingProducts($id){
+
+        return static::where('user_id', '=', $id)
+        ->where(function ($query) {
+            $query->where('status', '=', 'Pending');
+        })
+        ->with(['product','user'])
+        ->orderBy('start_date','desc')
+        ->get();
+        
+    }
+
+     //All pending subscriptions
+     public static function pendingSubs(){
+
+        return static::where('status','Pending')
+        ->oldest()
+        
+        ->with(['product','user'])
+        ->paginate(100);
+        }
+
+        //All active subscriptions
+        public static function activeSubs(){
+            return static::where('status','Active')
+            ->orderBy('user_id','asc')
+            ->orderBy('start_date','asc')
+            ->with(['product','user'])
+            ->paginate(100);
+            }
+
 }

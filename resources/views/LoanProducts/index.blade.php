2f9d51d9340e9f6279ea5538bd2570dc915d3f86
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:07:40 +0100

    edit heading display

--- resources/views/LoanProducts/index.blade.php
+++ resources/views/LoanProducts/index.blade.php
@@ -5,9 +5,13 @@
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <p class="teal-text">All Loan Products</p>
+            <p class="teal-text">ALL LOAN PRODUCTS</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 subject-header">
             <span><a href="/loanProduct/create"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Loan Product">playlist_add</i></a></span>
-            <span><a href="/new-subscription"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Product Subscription">add_shopping_cart</i></a></span>
+            <span><a href="/loanProduct/create"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Loan Product">add</i></a></span>
         </div>
     </div>
 
@@ -20,6 +24,7 @@
                         <th>Loan Name</th>
                         <th>Tenor (Months)</th>
                         <th>Interest Rate</th>
+                        <th>Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -28,7 +33,7 @@
                         <td><a href="/loanProduct/edit/{{$item->id}}">{{$item->description}}</a></td>
                         <td>{{$item->tenor}}</td>
                         <td>{{$item->interest}}</td>
-                        <td><a href="/new-subscription">Subscribe</a></td>
+                        <td><a href="/loanSub/create">Subscribe</a></td>
                     </tr>
                     @endforeach
                 </tbody>

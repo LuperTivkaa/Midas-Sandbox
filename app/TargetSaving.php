bfdbf9fd5ba78fb9d7276dec45b68d665ad7820f
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:18:32 +0100

    method to find sum target saving

--- app/TargetSaving.php
+++ app/TargetSaving.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Targetsr;
 
 class Targetsaving extends Model
 {
@@ -18,6 +19,32 @@ class Targetsaving extends Model
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Relationship for target saving review
+     * Each Target saving belongs to a Target saving review
+     */
+    public function targetsavingreview(){
+        return $this->belongsTo(Targetsr::class);
+    }
+
 protected $dates = ['created_at', 'updated_at','start_date','entry_date','end_date'];
+
+/**
+ * Find the sum of user target saving
+ * pass in the user id
+ */
+public static function myTargetSavings($id){
+    /**
+     * first find the target saving review that is active where user id is id
+     */
+    $tr = Targetsr::where('user_id',$id)
+    ->where(function ($query) {
+    $query->where('status', 'Active');
+    })->get();
+    foreach($tr as $t){
+        $targetsr_id = $t->id;
+    }
+    return TargetSaving::where('targetsr_id',$targetsr_id)->sum('amount');
+}
 
 }

8fd449b5bce8ded34204d7df387aed8c4f44085d
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:19:06 +0100

    aadd relationship with tarhet saving

--- app/Targetsr.php
+++ app/Targetsr.php
@@ -11,5 +11,13 @@ class Targetsr extends Model
      public function user(){
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * Relationship with Target saving
+     */
+    
+    public function targetsavings(){
+        return $this->hasMany(TargetSaving::class);
+    }
     protected $dates = ['created_at', 'updated_at','review_date'];
 }

acd32359a446d5d9ea34fd0c60d15a45e937e1f0
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:43:29 +0100

    add functionality product magt

--- app/Http/Controllers/ProductSubscriptionController.php
+++ app/Http/Controllers/ProductSubscriptionController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Product;
 use App\User;
 use App\Psubscription;
+use Carbon\Carbon;
 
 class ProductSubscriptionController extends Controller
 {
@@ -18,7 +19,10 @@ class ProductSubscriptionController extends Controller
     {
         //List all Subscriptions
         $title ='All Product Subscriptions';
-        $prod = Product::withCount('psubscriptions')->paginate(5);
+
+        $prod = Product::withCount(['psubscriptions' => function ($query){
+            $query->where('status','Pending');
+        }])->paginate(5);
         return view('ProductSub.index',compact('prod','title'));
         // $users = DB::table('users')
         //              ->select(DB::raw('count(*) as user_count, status'))
@@ -51,26 +55,33 @@ class ProductSubscriptionController extends Controller
             $this->validate(request(), [
                 'product' =>'required|integer',
                 'payment_id'=>'required|integer',
-                'units' =>'required|integer',
+                'units' => 'required|integer|between:1,60',
+                'guarantor_id' => 'required|integer',
+                'net_pay' =>'required|numeric|between:0.00,999999999.99',
                 ]);
     
                 if(User::where('payment_number',request(['payment_id']))->exists())
                 {
                     $user = User::where('payment_number',request(['payment_id']))->first();
                     $user_id = $user->id;
+                    $product = Product::find($request['product']);
+                    //Total subscription cost
+                    $productSubTotalCost = Psubscription::totalProductCost($product->unit_cost, $request['units']);
+                    $productTenor = $product->tenor;
                     $product_sub = new Psubscription();
                     $product_sub->user_id = $user_id;
                     $product_sub->product_id = $request['product'];
+                    $product_sub->monthly_repayment = $productSubTotalCost/$productTenor;
+                    $product_sub->total_amount = $productSubTotalCost;
+                    $product_sub->guarantor_id = User::userID(request(['guarantor_id']));
                     $product_sub->units = $request['units'];
-                    $product_sub->start_date = $request['start_date'];
-                    $product_sub->end_date = $request['end_date'];
+                    $product_sub->net_pay = $request['net_pay'];
                     $product_sub->staff_id = auth()->id();
                     $product_sub->save();
                     if($product_sub->save()) {
                         toastr()->success('Product Subscription has been saved successfully!');
                         return redirect('/subscriptions');
                     }
-                
                     toastr()->error('An error has occurred trying to create a subscription.');
                     return back();
                 }
@@ -87,9 +98,14 @@ class ProductSubscriptionController extends Controller
      */
     public function show($id)
     {
-        //Display detail product subscription listings
+          //Display detail product subscription listings
           $title ='Detail Product Subscriptions';
-          $subs = Psubscription::where('product_id',$id)->with(['product' => function ($query) {
+
+          $subs = Psubscription::where('product_id',$id)
+          ->where(function ($query){
+              $query->where('status','Pending');
+          })
+          ->with(['product' => function ($query) {
             $query->orderBy('name', 'desc');
         }])->paginate(10);
           
@@ -140,24 +156,32 @@ class ProductSubscriptionController extends Controller
         $this->validate(request(), [
             'product' =>'required|integer',
             'payment_id'=>'required|integer',
-            'units' =>'required|integer',
+            'units' => 'required|integer|between:1,60',
+            //'guarantor_id' => 'required|integer',
+            'net_pay' =>'required|numeric|between:0.00,999999999.99',
             ]);
 
             if(User::where('payment_number',request(['payment_id']))->exists())
             {
                 $user = User::where('payment_number',request(['payment_id']))->first();
                 $user_id = $user->id;
+                $product = Product::find($request['product']);
+                //Total subscription cost
+                $productSubTotalCost = Psubscription::totalProductCost($product->unit_cost, $request['units']);
+                $productTenor = $product->tenor;
                 $product_sub = Psubscription::find($id);
                 $product_sub->user_id = $user_id;
                 $product_sub->product_id = $request['product'];
+                $product_sub->monthly_repayment = $productSubTotalCost/$productTenor;
+                $product_sub->total_amount = $productSubTotalCost;
+                //$product_sub->guarantor_id = User::userID(request(['guarantor_id']));
                 $product_sub->units = $request['units'];
-                $product_sub->start_date = $request['start_date'];
-                $product_sub->end_date = $request['end_date'];
+                $product_sub->net_pay = $request['net_pay'];
                 $product_sub->staff_id = auth()->id();
                 $product_sub->save();
                 if($product_sub->save()) {
                     toastr()->success('Product Subscription updated successfully!');
-                    return redirect('/subscriptions');
+                    return redirect('/prodSub/pending');
                 }
             
                 toastr()->error('An error has occurred trying to update user subscription.');
@@ -179,7 +203,7 @@ class ProductSubscriptionController extends Controller
         $dlt = Psubscription::find($id)->delete();
         if($dlt) {
             toastr()->success('Product subscription deleted successfully!');
-            return redirect('/subscriptions');
+            return redirect('/prodSub/pending');
         }
     
         toastr()->error('An error has occurred trying to delete user subscription.');
@@ -202,5 +226,72 @@ class ProductSubscriptionController extends Controller
             return view('ProductSub.active',compact('activeSubs','title'));
             }
 
+        //stop subscription
+        public function subStop($id){
+
+            $mySub = Psubscription::find($id);
+            
+            $sumOfProduct = $mySub->total_amount;
+            //3 get sum deductions for the product
+            $totalDeductions = $mySub->totalSubDeductions($id);
+            //find the diff
+            $diffRslt = $sumOfProduct-$totalDeductions;
+            if($diffRslt <= 0){
+                //update the subj obj status to inactive
+                //retrun to active Sub page
+                $mySub->status = 'Inactive';
+                $mySub->end_date = now()->toDateString();
+                $mySub->review_by = auth()->id();
+                    $mySub->save();
+                    if($mySub->save()) {
+                        toastr()->success('This subscription has been successfully stop');
+                        return redirect('/user/page/'.$mySub->user_id);
+                    }
+            }
+            toastr()->error('You can not stop this facility, please check details');
+                    return back();
+
+            }
+
+            //Review product subscription
+            public function review($id){
+            $title ='Review Loan Subscription';
+            $review = Psubscription::find($id);
+            return view('ProductSub.reviewProdSub',compact('review','title'));
+            }
+
+//Store Product Review
+public function reviewStore(Request $request, $id){
+     //Save product subscription
+     $this->validate(request(), [
+         'start_date' => 'required|date',
+         'end_date' => 'required|date',
+        'units' => 'required|integer|between:1,60',
+        'notes' =>'required|string',
+        ]);
+
+            $product_sub = Psubscription::find($id);
+            $product_tenor = $product_sub->product->tenor;
+            $product_unitCost = $product_sub->product->unit_cost;
+            //Total subscription cost
+            $productSubTotalCost = Psubscription::totalProductCost($product_unitCost, $request['units']);
+           
+            $product_sub->monthly_repayment = $productSubTotalCost/$product_tenor;
+            $product_sub->total_amount = $productSubTotalCost;
+            $product_sub->units = $request['units'];
+            $product_sub->status = 'Active';
+            $product_sub->staff_id = auth()->id();
+            $product_sub->save();
+            if($product_sub->save()) {
+                toastr()->success('Product Subscription has been reviewed successfully!');
+                return redirect('/prodSub/pending');
+            }
+            toastr()->error('An error has occurred trying to review a subscription.');
+            return back();
+   
+}
+
     
+
+
 }

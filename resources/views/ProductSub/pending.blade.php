a01bcc0b8cc26c7eb8e2cb221ae68014164be42b
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:13:28 +0100

    removed markup

--- resources/views/ProductSub/pending.blade.php
+++ resources/views/ProductSub/pending.blade.php
@@ -1,4 +1,5 @@
-@extends('Layouts.admin-app') 
+@extends('Layouts.admin-app')
+
 @section('main-content')
 <div class="container">
     {{--
@@ -11,7 +12,8 @@
     </div>
     <div class="row">
         <div class="col s12 subject-header">
-            <span><a href="/loanSub/create"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Loan Subscription">playlist_add</i></a></span>
+            <span><a href="/loanSub/create"><i class="small material-icons tooltipped" data-position="bottom"
+                        data-tooltip="New Loan Subscription">playlist_add</i></a></span>
         </div>
     </div>
 
@@ -23,34 +25,28 @@
                     <tr>
                         <th>Name</th>
                         <th>Product</th>
-                        <th>Units</th>
-                        <th>x-item</th>
-                        <th>Sum NGN</th>
-                        <th>Monthly Repay</th>
                         <th>Created On</th>
                         <th>Status</th>
+                        <th>Active</th>
                     </tr>
                 </thead>
                 <tbody>
                     @foreach ($pendingSubs as $pending)
                     <tr>
 
-                        <td><a href="/user/page/{{$pending->user_id}}">{{$pending->user->first_name}} {{$pending->user->lastname_name}}</a></td>
+                        <td><a href="/user/page/{{$pending->user_id}}">{{$pending->user->first_name}}
+                                {{$pending->user->lastname_name}}</a></td>
                         <td>{{$pending->product->name}}</td>
-                        <td>{{$pending->units}}</td>
-                        <td>{{$pending->product->unit_cost}}</td>
-                        <td>{{number_format($pending->total_amount,2,'.',',')}}</td>
-                        <td>{{number_format($pending->monthly_repayment,2,'.',',')}}</td>
                         <td>{{$pending->created_at->toFormattedDateString()}}</td>
-
                         <td>{{$pending->status}}</td>
-                        <td><a href="/#/#/{{$pending->id}}">Repay</a> <a href="/#/#/{{$pending->id}}">Stop</a> </td>
+                        <td><a href="/prodSub/review/{{$pending->id}}">Review</a> <a
+                                href="/userProdSub/delete/{{$pending->id}}" class="red-text darken-4">Discard</a> </td>
                     </tr>
                     @endforeach
                 </tbody>
             </table>
             {{$pendingSubs->links()}} @else
-            <p>No Active Product Subscriptions</p>
+            <p>No Pending Product Subscriptions</p>
             @endif
         </div>
     </div>

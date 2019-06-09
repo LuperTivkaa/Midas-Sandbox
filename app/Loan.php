14607ef6b16f58719f960467f3ec9c5339d37592
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:16:13 +0100

    add loan deduction relationship

--- app/Loan.php
+++ app/Loan.php
@@ -13,6 +13,11 @@ class Loan extends Model
         return $this->hasMany(Lsubscription::class);
     }
 
+     //A loan may have many deductions
+     public function loandeductions(){
+        return $this->hasMany(Ldeduction::class);
+    }
+
     //loan products
     public static function loanProducts(){
         return static::orderBy('description')

eccc1332d5647c94b945e05213d8613243b6105b
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:15:13 +0100

    new print csss  assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,8 @@ mix.js("resources/js/app.js", "public/js")
     .sass("resources/sass/app.scss", "public/css")
     .sass("resources/sass/midas-styles.scss", "public/css")
     .sass("resources/sass/portal.scss", "public/css")
+    .sass("resources/sass/print.scss", "public/css")
+    .sass("resources/sass/printpdf.scss", "public/css")
     .copy(
         "node_modules/toastr/build/toastr.min.css",
         "public/css/toastr.min.css"

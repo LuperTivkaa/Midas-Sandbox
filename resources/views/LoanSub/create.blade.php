06a642a5f66d78dda734288a0bea0325a6347fd6
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:08:11 +0100

    Edit heading

--- resources/views/LoanSub/create.blade.php
+++ resources/views/LoanSub/create.blade.php
@@ -1,41 +1,57 @@
-@extends('Layouts.admin-app') 
+@extends('Layouts.admin-app')
+
 @section('main-content')
 <div class="container">
     {{--
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <span class="teal-text">New  Loan Subscription</span>
-            <span><a href="/loan-subscriptions"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Loan Subscription">view_list</i></a></span>
+            <span class="teal-text">NEW LOAN</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s12 subject-header">
+            <span><a href="/pendingLoans"><i class="small material-icons tooltipped" data-position="bottom"
+                        data-tooltip="Pending Loans">view_list</i></a></span>
         </div>
     </div>
     <div class="row">
         <form class="col s12" method="POST" action="/loanSub/store">
             {{ csrf_field() }}
             <div class="row">
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 m4 l4">
                     <input placeholder="IPPIS or GFMIS" id="payment_id" name="payment_id" type="text" class="validate">
                     <label for="payment_id">Payment ID</label>
                 </div>
 
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 m4 l4">
+                    <input placeholder="GUARANTOR IPPIS" id="guarantor_id" name="guarantor_id" type="text"
+                        class="validate">
+                    <label for="guarantor_id">Guarantor</label>
+                </div>
+
+                <div class="input-field col s12 m4 l4">
                     <select id="loan_product" name="loan_product">
-                            {{-- <option value="" disabled>Choose role</option> --}}
-                            @foreach ($loanProd as $id=>$description)
-                            <option value="{{$id}}">{{$description}}</option>
-                            @endforeach
-                        </select>
+                        {{-- <option value="" disabled>Choose role</option> --}}
+                        @foreach ($loanProd as $id=>$description)
+                        <option value="{{$id}}">{{$description}}</option>
+                        @endforeach
+                    </select>
                     <label>Loan Product</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 m4 l4">
                     <input id="amount_applied" name="amount_applied" type="text" class="validate">
                     <label for="amount_applied">Amount Applied</label>
                 </div>
-
-                <div class="input-field col s12 m6 l6">
-                    <input id="custom_tenor" name="custom_tenor" type="text" placeholder="Eg 3 or 5 (values in months Optional)">
+                <div class="input-field col s12 m4 l4">
+                    <input id="net_pay" name="net_pay" type="text" class="validate">
+                    <label for="net_pay">Net Pay</label>
+                </div>
+                <div class="input-field col s12 m4 l4">
+                    <input id="custom_tenor" name="custom_tenor" type="text"
+                        placeholder="Eg 3 or 5 (values in months Optional)">
                     <label for="custom_tenor">Custom Tenor</label>
                 </div>
             </div>

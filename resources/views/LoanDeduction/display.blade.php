a0d53102f07d009a7ed594cb3850770a07377439
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:04:52 +0100

    minor display edits

--- resources/views/LoanDeduction/display.blade.php
+++ resources/views/LoanDeduction/display.blade.php
@@ -1,22 +1,26 @@
 <table class="highlight">
     <thead>
         <tr>
-
             <th>Name</th>
             <th>Loan</th>
             <th>Monthly RePay</th>
             <th>Due</th>
-            <th>Detail</th>
+            <th>History</th>
         </tr>
     </thead>
     <tbody>
         @foreach ($loanSub as $active)
         <tr>
-            <td>{{$active->user->first_name}} {{$active->user->last_name}}</td>
+            <td>
+                <a href="/user/page/{{$active->user_id}}">{{$active->user->first_name}} {{$active->user->last_name}}</a>
+            </td>
             <td>{{$active->loan->description}}</td>
             <td>{{number_format($active->monthly_deduction,2,'.',',')}}</td>
-            <td>{{$active->loan_end_date->toDateString()}}</td>
-            {{-- <td><a href="/#/{{$active->id}}">More</a></td> --}}
+            <td>@if($active->loan_end_date=="") @else
+                {{$active->loan_end_date->toDateString()}}
+                @endif
+            </td>
+            <td><a href="/#/{{$active->id}}">History</a></td>
         </tr>
         @endforeach
     </tbody>

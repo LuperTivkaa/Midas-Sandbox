9eea84ce63a9b652879990a7703addddb818da95
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:14:52 +0100

    add display info

--- resources/views/Products/productDetail.blade.php
+++ resources/views/Products/productDetail.blade.php
@@ -5,27 +5,33 @@
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <p class="teal-text">Product Detail</p>
+            <p class="teal-text">PRODUCT DETAILS</p>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col s12 subject-header">
             <span><a href="/product/create"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Create Product">playlist_add</i></a></span>
             <span><a href="/products"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="All Products">view_list</i></a></span>
         </div>
     </div>
+
     <div class="row user-profiles">
         <div class="col s12 m3 l3 profile">
             {{-- <img src="{{asset('images/andy.jpg')}}" alt="" class="circle"> --}}
             <p class="profile__heading text-grey darken-3">Bird View</p>
-            <img src="{{asset('images/andy.jpg')}}" alt="" class="profile__photo materialboxed">
-            <span class="profile__user-name">  Product Avatar Above </span>
+            {{-- <img src="{{asset('images/andy.jpg')}}" alt="" class="profile__photo materialboxed"> --}} {{-- <span class="profile__user-name">  Product Avatar Above </span>            --}}
 
             <div class="profile__user-box">
                 <span class="black-text sub-profile">Added On</span>
-                <span class="profile__user-date grey-text lighten-2">{{$product->created_at->toFormattedDateString()}}</span>
+                <span class="profile__user-date grey-text lighten-2">
+                {{$product->created_at->toFormattedDateString()}}
+                </span>
                 <span class="black-text sub-profile">Subscriptions</span>
-                <span class="profile__join-date grey-text lighten-2">109</span>
-                <span class="black-text sub-profile">Status</span>
-                <span class="profile__user-status grey-text lighten-2">Active</span>
+                <h4 class="profile__join-date grey-text lighten-2">{{$product->productSubCount($product->id)}}</h4> {{-- <span class="black-text sub-profile"></span>
+                <span class="profile__user-status grey-text lighten-2"></span> --}}
             </div>
-            <span><a href="/editProduct/{{$product->id}}" class="pink-text darken-2">Edit</a></span>
+            <span><a href="/p-sub/{{$product->id}}" class="pink-text darken-2">More</a></span>
         </div>
         <div class="col s12 m9 l9 profile-detail">
             @if ($product)

e8c004fe632c825ec8fb9a46760d18032a1674c1
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 12:20:43 +0100

    add constructor properties

--- app/Exports/IppisLoandeductionsExport.php
+++ app/Exports/IppisLoandeductionsExport.php
@@ -7,6 +7,8 @@ use App\User;
 use App\Product;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
+// use Maatwebsite\Excel\Concerns\FromQuery;
+// use Maatwebsite\Excel\Concerns\Exportable;
 //use Maatwebsite\Excel\Concerns\FromCollection;
 
 class IppisLoandeductionsExport implements FromView
@@ -19,10 +21,26 @@ class IppisLoandeductionsExport implements FromView
     //     return Lsubscription::all();
     // }
 
+    // public function view():View
+    // {
+    //     $loanSub = Lsubscription::distinctUserLoanSub();
+
+    //     return view('LoanDeduction.ippisdownload',compact('loanSub'));
+    // }  
+    public function __construct($pay_type,$start_date,$end_date){
+        $this->pay_type = $pay_type;
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
+    }
+
     public function view():View
     {
-        $loanSub = Lsubscription::distinctUserLoanSub();
+        //$loanSub = Lsubscription::loanSubscriptions();
+        $premResult = Lsubscription::whereBetween('created_at',[$this->start_date,$this->end_date])
+        ->with(['loan','user'])->get();
+        $loanSub = $premResult->where('repayment_mode',$this->pay_type)->unique('user_id');
 
         return view('LoanDeduction.ippisdownload',compact('loanSub'));
-    }  
+    } 
+  
 }

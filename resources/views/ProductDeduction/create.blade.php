c7d9de1848e78a666c6a66465f16229d42ddce7f
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:10:48 +0100

    edit input fields

--- resources/views/ProductDeduction/create.blade.php
+++ resources/views/ProductDeduction/create.blade.php
@@ -5,12 +5,12 @@
     @include('inc.messages') --}}
     <div class="row">
         <div class="col s12 subject-header">
-            <h5 class="teal-text">CREATE PRODUCT REPAYMENT</h5>
+            <h5 class="teal-text">PRODUCT REPAYMENT</h5>
         </div>
     </div>
     <div class="row">
         <div class="col s12 subject-header">
-            <span><a href="/productDeduction/listings"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Go Back">arrow_back</i></a></span>
+            <span><a href="/prodSub/active"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Go Back">arrow_back</i></a></span>
 
         </div>
     </div>
@@ -38,37 +38,45 @@
                     <label>Product List</label>
                 </div>
 
-                <div class="input-field col s12">
+                <div class="input-field col s12 m6 l6">
 
                     <input id="amount" name="amount" type="text" class="validate">
-                    <label for="amount">Payment</label>
+                    <label for="amount">Enter Amount</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12 m4 l4">
+                <div class="input-field col s12 m3 l3">
                     <input id="bank_name" name="bank_name" type="text" class="validate">
                     <label for="bank_name">Bank Name</label>
                 </div>
-                <div class="input-field col s12 m4 l4">
-                    <input id="depositor_name" name="depositor_name" type="text">
+                <div class="input-field col s12 m3 l3">
+                    <input id="bank_add" name="bank_add" type="text" class="validate">
+                    <label for="bank_add">Bank Add</label>
+                </div>
+                <div class="input-field col s12 m3 l3">
+                    <input id="depositor_name" name="depositor_name" type="text" class="validate">
                     <label for="depositor_name">Depositor Name</label>
                 </div>
-                <div class="input-field col s12 m4 l4">
-                    <input id="teller_number" name="teller_number" type="text">
+                <div class="input-field col s12 m3 l3">
+                    <input id="teller_number" name="teller_number" type="text" class="validate">
                     <label for="teller_number">Teller Number</label>
                 </div>
             </div>
             <div class="row">
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 m4 l4">
                     <select id="mode" name="mode">
-                                <option value="Bank">Bank</option>
-                            </select>
+                            <option value="Bank">Bank</option>
+                    </select>
                     <label>Payment Mode</label>
                 </div>
 
-                <div class="input-field col s12 m6 l6">
+                <div class="input-field col s12 m4 l4">
                     <input id="entry_date" name="entry_date" type="text" class="validate datepicker">
                     <label for="entry_date">Date</label>
+                </div>
+                <div class="input-field col s12 m4 l4">
+                    <input id="notes" name="notes" type="text" class="validate">
+                    <label for="notes">Notes</label>
                 </div>
 
             </div>

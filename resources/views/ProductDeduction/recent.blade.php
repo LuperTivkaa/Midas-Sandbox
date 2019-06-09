ac775db2b90c914790b978edbf7388d9a849c1d2
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-09 13:11:08 +0100

    edit links

--- resources/views/ProductDeduction/recent.blade.php
+++ resources/views/ProductDeduction/recent.blade.php
@@ -1,4 +1,4 @@
-@extends('Layouts.admin-app') 
+@extends('Layouts.admin-app')
 @section('main-content')
 <div class="container">
     {{--
@@ -10,10 +10,13 @@
     </div>
     <div class="row">
         <div class="col s12 subject-header">
-            <span><a href="/"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Loan Subscription">playlist_add</i></a></span>
-            <span><a href="/saving/search"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="Search Savings">search</i></a></span>
+            <span><a href="/"><i class="small material-icons tooltipped" data-position="bottom"
+                        data-tooltip="New Loan Subscription">playlist_add</i></a></span>
+            <span><a href="/saving/search"><i class="small material-icons tooltipped" data-position="bottom"
+                        data-tooltip="Search Savings">search</i></a></span>
 
-            <span><a href="{{route('prod-deductions.upload')}}"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Savings Upload">cloud_upload</i></a></span>
+            <span><a href="{{route('prod-deductions.upload')}}"><i class="small material-icons tooltipped"
+                        data-position="bottom" data-tooltip="New Savings Upload">cloud_upload</i></a></span>
         </div>
     </div>
 
@@ -24,8 +27,8 @@
                 <thead>
                     <tr>
                         <th>Name</th>
-                        <th>Amount</th>
                         <th>Product</th>
+                        <th>Amount</th>
                         <th>Entry Date</th>
                         <th>Created At</th>
                         <th>Edit</th>
@@ -35,16 +38,20 @@
                 <tbody>
                     @foreach ($recent as $item)
                     <tr>
-                        <td><a href="/user/page/{{$item->user->id}}">{{$item->user->first_name}} {{$item->user->last_name}}</a></td>
+                        <td><a href="/user/page/{{$item->user->id}}">{{$item->user->first_name}}
+                                {{$item->user->last_name}}</a></td>
                         <td>{{$item->product->name}}</td>
-                        <td><a href="/productDeduction/detail/{{$item->id}}">{{number_format($item->monthly_deduction,2,'.',',')}}</a></td>
+                        <td><a
+                                href="/productDeduction/detail/{{$item->id}}">{{number_format($item->monthly_deduction,2,'.',',')}}</a>
+                        </td>
                         <td>{{$item->entry_date->toDateString()}}</td>
                         <td>{{$item->created_at->diffForHumans()}}</td>
                         <td>
                             <a href="/productDeduction/edit/{{$item->id}}"><i class="small material-icons">edit</i> </a>
                         </td>
                         <td>
-                            <a href="/productDeduction/remove/{{$item->id}}" id="delete"> <i class="small material-icons red-text">delete</i></a>
+                            <a href="/productDeduction/remove/{{$item->id}}" id="delete"> <i
+                                    class="small material-icons red-text">delete</i></a>
                         </td>
 
                     </tr>

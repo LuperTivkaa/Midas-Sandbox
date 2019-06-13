ecb4e9888af1a2486cce6872b985627ff7cf5079
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-13 17:16:41 +0100

    Added role field

--- resources/views/Registration/newUser.blade.php
+++ resources/views/Registration/newUser.blade.php
@@ -44,6 +44,7 @@
                 @can('all')
                 <div class="input-field col s6">
                     <select id="role" name="role">
+                        <option value="" selected disabled>Cooperator</option>
                         @foreach ($roles as $id=>$role)
                         <option value="{{$id}}">{{$role}}</option>
                         @endforeach
@@ -101,14 +102,18 @@
 
             </div>
             <div class="row">
-                <div class="input-field col s4">
+                <div class="input-field col s3">
                     <select id="member_type" name="member_type">
                         <option value="Ordinary">Ordinary</option>
                         <option value="Associate">Associate</option>
                     </select>
                     <label>Membership Type</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s3">
+                    <input id="dofa" name="dofa" type="text" class="validate datepicker" required>
+                    <label for="dofa">DOFA</label>
+                </div>
+                <div class="input-field col s3">
                     <select id="employ_type" name="employ_type">
                         <option value="Permanent">Permanent</option>
                         <option value="Temporal">Temporal</option>
@@ -116,7 +121,7 @@
                     </select>
                     <label>Employment Type</label>
                 </div>
-                <div class="input-field col s4">
+                <div class="input-field col s3">
                     <select id="job_cadre" name="job_cadre">
                         <option value="Senior">Senior</option>
                         <option value="Junior">Junior</option>
@@ -128,8 +133,6 @@
             </div>
 
             <div class="row">
-
-
 
                 <div class="input-field col s4">
                     <input id="dept" name="dept" type="text" class="validate" required>

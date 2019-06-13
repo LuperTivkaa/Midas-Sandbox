0f0545e207ebe7fb076741e901361eac14a732ab
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-13 17:27:08 +0100

    Edited user form

--- resources/views/inc/admin-top-section.blade.php
+++ resources/views/inc/admin-top-section.blade.php
@@ -1,10 +1,10 @@
 <section class="section section-stats center">
     <div class="row search_bar">
-        <form class="customsearch" method="POST" action="/Dashboard/user/customsearch">
+        <form class="customsearch" method="POST" action="/search/User">
             {{ csrf_field() }}
 
-            <div class="customsearch__item"><input class="custom-input custom-width" type="text" name="from" id="from"
-                    placeholder="Provide IPPIS or Surname">
+            <div class="customsearch__item"><input class="custom-input custom-width" type="text" name="search_term"
+                    id="search_term" placeholder="Provide IPPIS or First Name">
             </div>
             <div class="customsearch__item"><button class="custom-input" type="submit">Search</button></div>
         </form>

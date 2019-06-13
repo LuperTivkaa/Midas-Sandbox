f7089e6d0121a962faf64585a9ccd1d2cd3b9c43
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-13 17:31:11 +0100

    local storage sym link files

---
+++ storage/framework/views/5878f6caf4e67ef83128cf923f19b0150839790e.php
@@ -0,0 +1,58 @@
+<section class="section section-stats center">
+    <div class="row search_bar">
+        <form class="customsearch" method="POST" action="/search/User">
+            <?php echo e(csrf_field()); ?>
+
+
+            <div class="customsearch__item"><input class="custom-input custom-width" type="text" name="search_term"
+                    id="search_term" placeholder="Provide IPPIS or First Name">
+            </div>
+            <div class="customsearch__item"><button class="custom-input" type="submit">Search</button></div>
+        </form>
+    </div>
+    <div class="row">
+        <div class="col s12 m6 l3">
+            <div class="card-panel blue lighten-1 white-text center">
+                <i class="material-icons">insert_emoticon</i>
+                <h6>Active Users</h6>
+                <h5 class="count">768</h5>
+                <div class="progress grey lighten-1">
+                    <div class="determinate white" style="width:40%;"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel teal lighten-1 white-text center">
+                <i class="material-icons">mode_edit</i>
+                <h6>Deductions</h6>
+                <h5 class="count">14768000</h5>
+                <div class="progress grey lighten-1">
+                    <div class="determinate white" style="width:40%;"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel blue lighten-1 white-text center">
+                <i class="material-icons">insert_emoticon</i>
+                <h6>Target Savings</h6>
+                <h5 class="count">12768000</h5>
+                <div class="progress grey lighten-1">
+                    <div class="determinate white" style="width:20%;"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s12 m6 l3">
+            <div class="card-panel blue lighten-1 white-text center">
+                <i class="material-icons">insert_emoticon</i>
+                <h6>Loan Applications</h6>
+                <h5 class="count">768</h5>
+                <div class="progress grey lighten-1">
+                    <div class="determinate white" style="width:40%;"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ storage/framework/views/da99db3230cf6baefa9207e5e9965008a24aca19.php
@@ -0,0 +1,154 @@
+<nav class="blue darken-4">
+
+    <div class="container">
+        <div class="nav-wrapper">
+            <ul class="right">
+                <?php if(Auth::check()): ?>
+                <li class="right"><?php echo e(auth()->user()->first_name); ?></li>
+                <?php endif; ?>
+            </ul>
+        </div>
+    </div>
+
+    <div class="container">
+        <div class="nav-wrapper">
+            <a href="/" class="brand-logo"><img height="30" src="<?php echo e(asset('images/logo2.svg')); ?>" alt="logo"></a>
+
+            <a href="#" data-target="slide-out" class="sidenav-trigger right show-on-large"><i
+                    class="material-icons">menu</i></a>
+
+            <ul class="right hide-on-med-and-down">
+
+                <li><a class="dropdown-trigger" href="/" data-target='dropdown1'><i
+                            class="material-icons left">group</i></a></li>
+                <li><a class="dropdown-trigger" href="/about" data-target='dropdown2'><i
+                            class="material-icons left">bubble_chart</i></a></li>
+                <li><a class="dropdown-trigger" href="/about" data-target='dropdown3'><i
+                            class="material-icons left">style</i></a></li>
+                <li><a class="dropdown-trigger" href="/about" data-target='dropdown4'><i
+                            class="material-icons left">person</i></a></li>
+                <li><a class="dropdown-trigger" href="/about" data-target='dropdown5'><i
+                            class="material-icons left">dashboard</i></a></li>
+            </ul>
+
+        </div>
+    </div>
+
+
+</nav>
+
+
+<ul id="slide-out" class="sidenav">
+    <li>
+        <div class="user-view">
+            <div class="background">
+                <img src="<?php echo e(asset('images/logo2.svg')); ?>">
+            </div>
+            <a href="#"><img class="circle" src="<?php echo e(asset('images/logo2.svg')); ?>"></a>
+            <a href="#"><span class="grey-text darken-3 name"><?php if(Auth::check()): ?>
+                    <?php echo e(auth()->user()->first_name); ?>, <?php echo e(auth()->user()->last_name); ?><?php endif; ?>
+                </span></a>
+            <a href="#"><span
+                    class="grey-text darken-3 email"><?php if(Auth::check()): ?><?php echo e(auth()->user()->email); ?><?php endif; ?></span></a>
+        </div>
+    </li>
+    <li><a class="subheader">Profile</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+
+    <li><a href="#!"><i class="material-icons">person</i>View</a></li>
+    <li><a href="#!"><i class="material-icons">settings</i>Change Password</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Cooperators</a></li>
+    <li><a href="/New"><i class="material-icons">add</i>New User</a></li>
+    <li><a href="/user/all"><i class="material-icons">view_list</i>List Users</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Monthly Savings</a></li>
+    <li><a href="#!"><i class="material-icons">create</i>New Savings</a></li>
+    <li><a href="#!"><i class="material-icons">add</i>New Template</a></li>
+    <li><a href="#!"><i class="material-icons">cloud_upload</i>Upload Deductions</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Product Schemes</a></li>
+    <li><a href="#!"><i class="material-icons">create</i>New Product</a></li>
+    <li><a href="#!"><i class="material-icons">view_list</i>All Products</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Target Savings</a></li>
+    <li><a href="#!"><i class="material-icons">create</i>New Target Saving</a></li>
+    <li><a href="#!"><i class="material-icons">cloud_download</i>New Template</a></li>
+    <li><a href="#!"><i class="material-icons">cloud_upload</i>Upload Target Saving</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="subheader">Staff</a></li>
+    <li><a href="#!"><i class="material-icons">create</i>New Staff</a></li>
+    <li><a href="#!"><i class="material-icons">view_list</i>Staff List</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a href="#!"><i class="material-icons">vpn_lock</i>Log Out</a></li>
+
+    
+</ul>
+
+
+
+<ul id='dropdown1' class='dropdown-content'>
+
+
+    <li><a href="/New"><i class="material-icons">add</i>New User</a></li>
+
+    <li><a href="/user/all"><i class="material-icons">view_list</i>List Users</a></li>
+
+
+    <li class="divider" tabindex="-1"></li>
+    
+</ul>
+
+
+
+<ul id='dropdown2' class='dropdown-content'>
+    <li><a href="#!"><i class="material-icons">create</i>New Savings</a></li>
+    <li><a href="#!"><i class="material-icons">add</i>New Template</a></li>
+    <li class="divider" tabindex="-1"></li>
+    
+    <li><a href="#!"><i class="material-icons">cloud_upload</i>Upload Deductions</a></li>
+    
+</ul>
+
+
+
+<ul id='dropdown3' class='dropdown-content'>
+    <li><a href="#!"><i class="material-icons">create</i>New Product</a></li>
+    <li><a href="#!"><i class="material-icons">view_list</i>All Products</a></li>
+    <li class="divider" tabindex="-1"></li>
+      
+</ul>
+
+
+
+<ul id='dropdown4' class='dropdown-content'>
+    <li><a href="#!"><i class="material-icons">create</i>New Target Saving</a></li>
+    <li><a href="#!"><i class="material-icons">cloud_download</i>New Template</a></li>
+    <li class="divider" tabindex="-1"></li>
+    
+    <li><a href="#!"><i class="material-icons">cloud_upload</i>Upload Target Saving</a></li>
+    
+</ul>
+
+
+
+<ul id='dropdown5' class='dropdown-content'>
+    <li><a href="#!"><i class="material-icons">create</i>New Staff</a></li>
+    <li><a href="#!"><i class="material-icons">view_list</i>Staff List</a></li>
+    <li class="divider" tabindex="-1"></li>
+      
+</ul>

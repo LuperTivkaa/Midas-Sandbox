1219a0d6c9061a0225622bd98d7307adb290011d
Tivkaa Luper <tivkaaluper@gmail.com>
2019-06-17 08:42:53 +0100

    Create new link

--- resources/views/ProductSub/active.blade.php
+++ resources/views/ProductSub/active.blade.php
@@ -1,4 +1,4 @@
-@extends('Layouts.admin-app') 
+@extends('Layouts.admin-app')
 @section('main-content')
 <div class="container">
     {{--
@@ -12,7 +12,8 @@
 
     <div class="row">
         <div class="col s12 subject-header">
-            <span><a href="/new-subscription"><i class="small material-icons tooltipped" data-position="bottom" data-tooltip="New Product Subscription">playlist_add</i></a></span>
+            <span><a href="/new-subscription"><i class="small material-icons tooltipped" data-position="bottom"
+                        data-tooltip="New Product Subscription">playlist_add</i></a></span>
         </div>
     </div>
 
@@ -36,15 +37,17 @@
                     @foreach ($activeSubs as $active)
                     <tr>
 
-                        <td><a href="/user/page/{{$active->user_id}}">{{$active->user->first_name}} {{$active->user->last_name}}</a></td>
+                        <td><a href="/user/page/{{$active->user_id}}">{{$active->user->first_name}}
+                                {{$active->user->last_name}}</a></td>
                         <td>{{$active->product->name}}</td>
                         <td>{{number_format($active->total_amount,2,'.',',')}}</td>
                         <td>{{number_format($active->totalSubDeductions($active->id),2,'.',',')}}</td>
-                        <td>{{number_format($active->total_amount-$active->totalSubDeductions($active->id),2,'.',',')}}</td>
+                        <td>{{number_format($active->total_amount-$active->totalSubDeductions($active->id),2,'.',',')}}
+                        </td>
                         <td>{{$active->start_date->toFormattedDateString()}}</td>
                         <td>{{$active->start_date->diffForHumans($active->end_date->toFormattedDateString())}}</td>
-                        <td><a href="/product/repay/{{$active->id}}" class="teal-text lighten-4">Repay</a> <a href="/#/#/{{$active->id}}"
-                                class="pink-text lighten-3">Stop</a> </td>
+                        <td><a href="/product/repay/{{$active->id}}" class="teal-text lighten-4">Repay</a> <a
+                                href="/prodSub/stop/{{$active->id}}" class="pink-text lighten-3">Stop</a> </td>
                     </tr>
                     @endforeach
                 </tbody>

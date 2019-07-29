0718cfc9caef094cd4b76aec96ae609c55be28e5
Tivkaa Luper <tivkaaluper@gmail.com>
2019-07-29 06:45:20 +0100

    reduced container width

--- resources/views/inc/admin-side-section.blade.php
+++ resources/views/inc/admin-side-section.blade.php
@@ -1,22 +1,38 @@
-<div class="col s12 m4 l3 side-section-detail">
+<div class="col s12 m2 l2 side-section-detail">
 
     <div class="user-profiles white side-section-inner">
-        <h6 class="profile__heading pink-test lighten-4">Loan Applications</h6>
-        <p class="sub-profile">Summary as at 23rd Feb, 2019</p>
-        <span class="sub-profile">Long Term</span>
-        <span><a href="">133</a></span>
-        <span class="sub-profile">Medium Term</span>
-        <span><a href="">45</a></span>
-        <span class="sub-profile">Short Term </span>
-        <span><a href="">67</a></span>
-        <span class="sub-profile">Emergency</span>
-        <span><a href="">0</a></span>
+        <h6 class="profile__heading pink-test lighten-4">Quick Links</h6>
+        <ul>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+        </ul>
+    </div>
+    <div class="user-profiles white side-section-inner">
+        <h6 class="profile__heading pink-test lighten-4">A</h6>
+        <ul>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+        </ul>
+    </div>
+    <div class="user-profiles white side-section-inner">
+        <h6 class="profile__heading pink-test lighten-4">B</h6>
+        <ul>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+            <li>Links Goes Here</li>
+        </ul>
     </div>
 
     <div class="user-profiles white side-section-inner">
-        <h6 class="profile__heading">Target Savings</h6>
-        <p class="sub-profile">Summary as at 23rd Feb, 2019</p>
-        <span><a href="">N 23,000,000</a></span>
+
     </div>
 
     {{--
@@ -25,12 +41,13 @@
         <ul class="collection with-header latest-comments">
             <li class="collection-item avatar">
                 <img src="{{asset('images/andy.jpg')}}" alt="" class="circle">
-                <span class="title">Andy Shimakaa</span>
-                <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio delectus quidem
-                    nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!
-                </p>
-                <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
-            </li>
-        </ul>
-    </div> --}}
+    <span class="title">Andy Shimakaa</span>
+    <p class="truncate">Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque non nam quaerat vel fugit odio
+        delectus quidem
+        nemo modi, cum, fuga odit tempore et quae beatae totam? Veniam, similique. Maxime!
+    </p>
+    <a href="#" class="approve green-text">Approve</a> | <a href="#" class="deny red-text">Deny</a>
+    </li>
+    </ul>
+</div> --}}
 </div>
